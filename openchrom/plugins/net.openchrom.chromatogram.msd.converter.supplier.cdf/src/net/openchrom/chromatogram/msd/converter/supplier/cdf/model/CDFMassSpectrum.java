64e206e4b08fbde88b864d27258d6e8cff4b1928
Philip (eselmeister) Wenig <philip.wenig@gmx.net>
2011-08-02 07:24:59 +0200

    There has something gone wrong replacing the term ion. It was case insensitive, that's why constant declarations have been renamed to e.g. SELECTED_ionS. It has been corrected.

--- openchrom/plugins/net.openchrom.chromatogram.msd.converter.supplier.cdf/src/net/openchrom/chromatogram/msd/converter/supplier/cdf/model/CDFMassSpectrum.java
+++ openchrom/plugins/net.openchrom.chromatogram.msd.converter.supplier.cdf/src/net/openchrom/chromatogram/msd/converter/supplier/cdf/model/CDFMassSpectrum.java
@@ -32,12 +32,12 @@ public class CDFMassSpectrum extends AbstractSupplierMassSpectrum implements ICD
 	private static final long serialVersionUID = 251851545112617476L;
 	private static final Logger logger = Logger.getLogger(CDFMassSpectrum.class);
 	/**
-	 * MAX_ionS The total amount of ions to be stored in the
+	 * MAX_IONS The total amount of ions to be stored in the
 	 * cdf chromatogram.<br/>
 	 * It does not mean, that m/z 65535 is the upper bound, but only 65535 mass
 	 * fragments can be stored in a mass spectrum.
 	 */
-	public static final int MAX_ionS = 65535;
+	public static final int MAX_IONS = 65535;
 	public static final int MIN_RETENTION_TIME = 0;
 	public static final int MAX_RETENTION_TIME = Integer.MAX_VALUE;
 
@@ -49,7 +49,7 @@ public class CDFMassSpectrum extends AbstractSupplierMassSpectrum implements ICD
 	@Override
 	public int getMaxPossibleIons() {
 
-		return MAX_ionS;
+		return MAX_IONS;
 	}
 
 	@Override
