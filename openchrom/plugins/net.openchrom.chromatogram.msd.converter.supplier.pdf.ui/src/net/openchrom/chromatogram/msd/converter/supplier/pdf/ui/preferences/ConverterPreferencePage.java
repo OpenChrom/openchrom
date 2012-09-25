293a2a868b407a198de9fad474d6b7c3813f445e
Philip (eselmeister) Wenig <philip.wenig@gmx.net>
2012-09-25 11:10:06 +0200

    The simple and extended key validators are in the net.opemnchrom.keys.ui plug-in available now. This makes it easier to maintain the functionality.

--- openchrom/plugins/net.openchrom.chromatogram.msd.converter.supplier.pdf.ui/src/net/openchrom/chromatogram/msd/converter/supplier/pdf/ui/preferences/ConverterPreferencePage.java
+++ openchrom/plugins/net.openchrom.chromatogram.msd.converter.supplier.pdf.ui/src/net/openchrom/chromatogram/msd/converter/supplier/pdf/ui/preferences/ConverterPreferencePage.java
@@ -7,14 +7,14 @@ package net.openchrom.chromatogram.msd.converter.supplier.pdf.ui.preferences;
 
 import net.openchrom.chromatogram.msd.converter.supplier.pdf.preferences.BundleProductPreferences;
 import net.openchrom.chromatogram.msd.converter.supplier.pdf.ui.Activator;
-import net.openchrom.keys.ui.preferences.AbstractFieldEditorKeyPreferencePage;
+import net.openchrom.keys.ui.preferences.AbstractCustomFieldEditorPreferencePage;
 import net.openchrom.keys.ui.preferences.IKeyPreferencePage;
 
-public class ConverterPreferencePage extends AbstractFieldEditorKeyPreferencePage implements IKeyPreferencePage {
+public class ConverterPreferencePage extends AbstractCustomFieldEditorPreferencePage implements IKeyPreferencePage {
 
 	public ConverterPreferencePage() {
 
-		super(Activator.getDefault().getPreferenceStore(), new BundleProductPreferences());
+		super(Activator.getDefault().getPreferenceStore(), new BundleProductPreferences(), false);
 	}
 
 	@Override
