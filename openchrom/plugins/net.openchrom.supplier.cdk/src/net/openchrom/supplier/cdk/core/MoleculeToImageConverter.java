63fade1ad19fcee8d7b2372102a705fe5ba8fcea
Dr. Philip Wenig <philip.wenig@gmx.net>
2013-10-24 16:12:20 +0200

    The header of the converter has been updated.

--- openchrom/plugins/net.openchrom.supplier.cdk/src/net/openchrom/supplier/cdk/core/MoleculeToImageConverter.java
+++ openchrom/plugins/net.openchrom.supplier.cdk/src/net/openchrom/supplier/cdk/core/MoleculeToImageConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 administrator_marwin.
+ * Copyright (c) 2013 Marwin Wollschläger.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * administrator_marwin - initial API and implementation
+ * Marwin Wollschläger - initial API and implementation
  *******************************************************************************/
 package net.openchrom.supplier.cdk.core;
 
