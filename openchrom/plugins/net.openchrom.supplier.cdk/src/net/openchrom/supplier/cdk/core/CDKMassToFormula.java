5f4147985efee1d8d13022efd31ec23ea96391ce
Dr. Philip Wenig <philip.wenig@gmx.net>
2013-11-19 17:44:01 +0100

    The CDK view uses 6 parts now.

--- openchrom/plugins/net.openchrom.supplier.cdk/src/net/openchrom/supplier/cdk/core/CDKMassToFormula.java
+++ openchrom/plugins/net.openchrom.supplier.cdk/src/net/openchrom/supplier/cdk/core/CDKMassToFormula.java
@@ -21,38 +21,33 @@ import org.openscience.cdk.interfaces.IMolecularFormula;
 import org.openscience.cdk.interfaces.IMolecularFormulaSet;
 
 public class CDKMassToFormula {
-	
-	//List <IMolecularFormula> formulas = new ArrayList<IMolecularFormula>();
+
+	// List <IMolecularFormula> formulas = new ArrayList<IMolecularFormula>();
 	public IMolecularFormulaSet generate(double mass) {
+
 		MassToFormulaTool t = new MassToFormulaTool(DefaultChemObjectBuilder.getInstance());
 		IMolecularFormulaSet result = t.generate(mass);
 		return result;
 	}
-	
-	public List<Double> getRatings(double actualMass, IMolecularFormulaSet formulas)
-	{
+
+	public List<Double> getRatings(double actualMass, IMolecularFormulaSet formulas) {
+
 		List<Double> ratings = new ArrayList<Double>();
-		for(int j = 0; j < formulas.size(); j++)
-		{
+		for(int j = 0; j < formulas.size(); j++) {
 			IMolecularFormula formula = formulas.getMolecularFormula(j);
-			
 			double mass = 0.0;
-			for(IIsotope isotope : formula.isotopes() ){
-				mass += isotope.getExactMass()*formula.getIsotopeCount(isotope);
+			for(IIsotope isotope : formula.isotopes()) {
+				mass += isotope.getExactMass() * formula.getIsotopeCount(isotope);
 			}
-			System.out.println("mass : " + mass + " of formula " +actualMass);
-			double deviation = Math.abs(mass-actualMass)/ (actualMass);// for example (99-100) = -1 / 100 => 1% deviation
-			ratings.add(1-deviation);
+			System.out.println("mass : " + mass + " of formula " + actualMass);
+			double deviation = Math.abs(mass - actualMass) / (actualMass);// for example (99-100) = -1 / 100 => 1% deviation
+			ratings.add(1 - deviation);
 		}
-		
 		return ratings;
 	}
-	
-	
-	
-	public List<String> getNames(IMolecularFormulaSet formulas)
-	{
-		
+
+	public List<String> getNames(IMolecularFormulaSet formulas) {
+
 		List<String> results = new ArrayList<String>();
 		//
 		for(int i = 0; i < formulas.size(); i++) {
@@ -65,8 +60,6 @@ public class CDKMassToFormula {
 		}
 		return results;
 	}
-	
-	
 
 	public static void main(String[] args) {
 
@@ -75,17 +68,11 @@ public class CDKMassToFormula {
 		IMolecularFormulaSet formulas = massToFormula.generate(mass);
 		List<String> names = massToFormula.getNames(formulas);
 		List<Double> ratings = massToFormula.getRatings(mass, formulas);
-		
-		for ( String name : names )
-		{
+		for(String name : names) {
 			System.out.println(name);
 		}
-		for ( Double rating : ratings )
-		{
+		for(Double rating : ratings) {
 			System.out.println(rating);
 		}
-		
-		
 	}
-	
 }
