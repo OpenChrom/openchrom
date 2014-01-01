c22a9398a95dceb1248f8c12acc28013dcc6069a
Dr. Philip Wenig <philip.wenig@gmx.net>
2014-01-01 20:01:33 +0100

    Happy new year 2014!

--- openchrom/plugins/net.openchrom.supplier.cdk.fragment.test/src/AllTests.java
+++ openchrom/plugins/net.openchrom.supplier.cdk.fragment.test/src/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2013 Philip (eselmeister) Wenig.
+ * Copyright (c) 2008, 2014 Philip (eselmeister) Wenig.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
--- openchrom/plugins/net.openchrom.supplier.cdk.ui/src/net/openchrom/supplier/cdk/ui/views/FormulaCalculatorView.java
+++ openchrom/plugins/net.openchrom.supplier.cdk.ui/src/net/openchrom/supplier/cdk/ui/views/FormulaCalculatorView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 Dr. Philip Wenig.
+ * Copyright (c) 2013, 2014 Dr. Philip Wenig.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
