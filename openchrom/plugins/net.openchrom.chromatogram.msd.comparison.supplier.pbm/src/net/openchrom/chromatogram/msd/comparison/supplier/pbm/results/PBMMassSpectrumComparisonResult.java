3f5f05d854edc3c8a85b475261cfe079a1e1c464
Dr. Philip Wenig <philip.wenig@openchrom.net>
2014-07-28 09:13:31 +0200

    The PBM comparator shall be published in an open source repo with the next release.

--- openchrom/plugins/net.openchrom.chromatogram.msd.comparison.supplier.pbm/src/net/openchrom/chromatogram/msd/comparison/supplier/pbm/results/PBMMassSpectrumComparisonResult.java
+++ openchrom/plugins/net.openchrom.chromatogram.msd.comparison.supplier.pbm/src/net/openchrom/chromatogram/msd/comparison/supplier/pbm/results/PBMMassSpectrumComparisonResult.java
@@ -2,6 +2,12 @@
  * Copyright (c) 2008, 2014 Philip (eselmeister) Wenig.
  * 
  * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip (eselmeister) Wenig - initial API and implementation
  *******************************************************************************/
 package net.openchrom.chromatogram.msd.comparison.supplier.pbm.results;
 
