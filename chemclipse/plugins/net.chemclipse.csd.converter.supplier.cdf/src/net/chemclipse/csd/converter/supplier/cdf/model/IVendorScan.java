ee96a92bf714d786430f391883da524f77b93890
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-04-26 19:56:32 +0200

    All FID packages have been renamed to the more generic description CSD (conductivity selective detector).

---
+++ chemclipse/plugins/net.chemclipse.csd.converter.supplier.cdf/src/net/chemclipse/csd/converter/supplier/cdf/model/IVendorScan.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Dr. Philip Wenig.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package net.chemclipse.csd.converter.supplier.cdf.model;
+
+import net.chemclipse.csd.model.core.IScanFID;
+
+public interface IVendorScan extends IScanFID {
+
+	/**
+	 * Stores the total signal.
+	 * 
+	 * @param totalSignal
+	 */
+	void setTotalSignal(float totalSignal);
+}
