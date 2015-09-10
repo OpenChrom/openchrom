4fd1870e5ed1007308cac8b17a7d45f67e983deb
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-09-10 09:17:29 +0200

    I've refactored the base and core feature definitions.

--- chemclipse/plugins/net.openchrom.chromatogram.msd.identifier.supplier.cdk.ui/src/net/openchrom/chromatogram/msd/identifier/supplier/cdk/ui/internal/provider/FormulaListTableComparator.java
+++ chemclipse/plugins/net.openchrom.chromatogram.msd.identifier.supplier.cdk.ui/src/net/openchrom/chromatogram/msd/identifier/supplier/cdk/ui/internal/provider/FormulaListTableComparator.java
@@ -11,8 +11,8 @@
  *******************************************************************************/
 package net.openchrom.chromatogram.msd.identifier.supplier.cdk.ui.internal.provider;
 
-import org.eclipse.chemclipse.swt.ui.viewers.AbstractRecordTableComparator;
-import org.eclipse.chemclipse.swt.ui.viewers.IRecordTableComparator;
+import org.eclipse.chemclipse.support.ui.swt.viewers.AbstractRecordTableComparator;
+import org.eclipse.chemclipse.support.ui.swt.viewers.IRecordTableComparator;
 import org.eclipse.jface.viewers.Viewer;
 
 import net.openchrom.chromatogram.msd.identifier.supplier.cdk.formula.NameAndRating;
