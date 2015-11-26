9a94dae3237f77522a6970999bea8b0deb995eb8
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-11-26 18:57:58 +0100

    Added perspective.

---
+++ openchrom/plugins/net.openchrom.msd.identifier.supplier.opentyper.ui/src/net/openchrom/msd/identifier/supplier/opentyper/ui/parts/CalculationPart.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Matthias Mailänder, Dr. Philip Wenig.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Matthias Mailänder - initial API and implementation
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.msd.identifier.supplier.opentyper.ui.parts;
+
+import javax.inject.Inject;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+public class CalculationPart {
+
+	@Inject
+	public CalculationPart(Composite composite) {
+
+		composite.setLayout(new FillLayout());
+		Button button = new Button(composite, SWT.PUSH);
+		button.setText("Calculate");
+		button.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+
+				System.out.println("OpenTyper is fantastic!");
+			}
+		});
+	}
+}
