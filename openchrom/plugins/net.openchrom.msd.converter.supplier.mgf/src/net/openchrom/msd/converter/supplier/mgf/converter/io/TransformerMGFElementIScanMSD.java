fb1271de141266213cbf34e154aac013d377dd86
Alexander Kerner <ak@silico-sciences.com>
2015-12-10 00:31:58 +0100

    [mgf] Fix handling of missing retention time

--- openchrom/plugins/net.openchrom.msd.converter.supplier.mgf/src/net/openchrom/msd/converter/supplier/mgf/converter/io/TransformerMGFElementIScanMSD.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mgf/src/net/openchrom/msd/converter/supplier/mgf/converter/io/TransformerMGFElementIScanMSD.java
@@ -5,11 +5,14 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Dr. Alexander Kerner - initial API and implementation
  *******************************************************************************/
 package net.openchrom.msd.converter.supplier.mgf.converter.io;
+
+import net.sf.bioutils.proteomics.peak.Peak;
+import net.sf.jmgf.MGFElement;
 
 import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.model.exceptions.AbundanceLimitExceededException;
@@ -17,26 +20,28 @@ import org.eclipse.chemclipse.msd.model.core.IScanMSD;
 import org.eclipse.chemclipse.msd.model.exceptions.IonLimitExceededException;
 import org.eclipse.chemclipse.msd.model.implementation.ScanMSD;
 
-import net.sf.bioutils.proteomics.peak.Peak;
-import net.sf.jmgf.MGFElement;
-
 public class TransformerMGFElementIScanMSD {
 
-	private final TransformerPeakIon transformer = new TransformerPeakIon();
 	private static final Logger logger = Logger.getLogger(TransformerMGFElementIScanMSD.class);
+	private final TransformerPeakIon transformer = new TransformerPeakIon();
 
-	public IScanMSD transform(MGFElement element) {
+	public IScanMSD transform(final MGFElement element) {
 
-		ScanMSD massSpectrum = new ScanMSD();
+		final ScanMSD massSpectrum = new ScanMSD();
 		massSpectrum.setIdentifier(element.getTitle());
-		massSpectrum.setRetentionTime(element.getRetentionTimeInSeconds());
-		massSpectrum.setRetentionIndex(element.getPeaks().get(0).getFractionIndex());
-		for(Peak p : element.getPeaks()) {
+		try {
+			final double retentionTimeInSeconds = Double.parseDouble(element.getElement(MGFElement.Identifier.RTINSECONDS));
+			final double scale = retentionTimeInSeconds * 1000;
+			massSpectrum.setRetentionTime((int)Math.round(scale));
+		} catch(final NumberFormatException e) {
+			logger.debug("No retention time available");
+		}
+		for(final Peak p : element.getPeaks()) {
 			try {
 				massSpectrum.addIon(transformer.transform(p));
-			} catch(IonLimitExceededException e) {
+			} catch(final IonLimitExceededException e) {
 				logger.warn(e);
-			} catch(AbundanceLimitExceededException e) {
+			} catch(final AbundanceLimitExceededException e) {
 				logger.warn(e);
 			}
 		}
