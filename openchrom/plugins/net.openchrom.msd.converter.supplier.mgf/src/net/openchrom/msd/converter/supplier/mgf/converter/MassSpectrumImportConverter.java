fb9408552327466564d2143af732f5c1303d3fb4
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-12-10 08:13:07 +0100

    I've fixed the logger.warn call.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.mgf/src/net/openchrom/msd/converter/supplier/mgf/converter/MassSpectrumImportConverter.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mgf/src/net/openchrom/msd/converter/supplier/mgf/converter/MassSpectrumImportConverter.java
@@ -15,8 +15,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import net.openchrom.msd.converter.supplier.mgf.converter.io.MGFReader;
-
 import org.eclipse.chemclipse.converter.exceptions.FileIsEmptyException;
 import org.eclipse.chemclipse.converter.exceptions.FileIsNotReadableException;
 import org.eclipse.chemclipse.logging.core.Logger;
@@ -26,6 +24,8 @@ import org.eclipse.chemclipse.msd.converter.processing.massspectrum.MassSpectrum
 import org.eclipse.chemclipse.msd.model.core.IMassSpectra;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
+
+import net.openchrom.msd.converter.supplier.mgf.converter.io.MGFReader;
 
 public class MassSpectrumImportConverter extends AbstractMassSpectrumImportConverter {
 
@@ -57,7 +57,7 @@ public class MassSpectrumImportConverter extends AbstractMassSpectrumImportConve
 				logger.warn(e);
 				processingInfo.addErrorMessage(DESCRIPTION, "The file is empty: " + file.getAbsolutePath());
 			} catch(final IOException e) {
-				logger.warn(e.getLocalizedMessage(), e);
+				logger.warn(e);
 				processingInfo.addErrorMessage(DESCRIPTION, "Something has gone completely wrong: " + file.getAbsolutePath());
 			}
 		}
