45ec1879996b6316f98e49bc2bcacf24cf005e2e
Dr. Philip Wenig <philip.wenig@openchrom.net>
2015-12-29 16:22:26 +0100

    The Mars code formatting guidelines have been applied.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.mgf.ui/src/net/openchrom/msd/converter/supplier/mgf/ui/preferences/PreferencePage.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mgf.ui/src/net/openchrom/msd/converter/supplier/mgf/ui/preferences/PreferencePage.java
@@ -20,7 +20,6 @@ import net.openchrom.msd.converter.supplier.mgf.ui.Activator;
 public class PreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
 	public PreferencePage() {
-
 		super(GRID);
 		setPreferenceStore(Activator.getDefault().getPreferenceStore());
 		setDescription("Mascot Generic Format Converter");
