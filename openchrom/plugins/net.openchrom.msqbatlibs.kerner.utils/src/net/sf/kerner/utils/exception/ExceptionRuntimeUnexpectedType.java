d7b3147c0cc46bb969f87b0cc147ddf2801405c0
Alexander Kerner <alexanderkerner24@gmail.com>
2016-01-09 22:11:55 +0100

    Introduce ExceptionRuntimeUnexpectedType

---
+++ openchrom/plugins/net.openchrom.msqbatlibs.kerner.utils/src/net/sf/kerner/utils/exception/ExceptionRuntimeUnexpectedType.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Lablicate UG (haftungsbeschränkt).
+ *
+ * All rights reserved.
+ *
+ * Contributors:
+ * Dr. Alexander Kerner - initial API and implementation
+ *******************************************************************************/
+package net.sf.kerner.utils.exception;
+
+public class ExceptionRuntimeUnexpectedType extends RuntimeException {
+
+	private static final long serialVersionUID = 1729348152174596405L;
+
+	public ExceptionRuntimeUnexpectedType() {
+		super();
+	}
+
+	public ExceptionRuntimeUnexpectedType(Object unexpectedType) {
+		super("Unexpected type " + unexpectedType.getClass());
+	}
+
+	public ExceptionRuntimeUnexpectedType(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ExceptionRuntimeUnexpectedType(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ExceptionRuntimeUnexpectedType(String message) {
+		super(message);
+	}
+
+	public ExceptionRuntimeUnexpectedType(Throwable cause) {
+		super(cause);
+	}
+}
