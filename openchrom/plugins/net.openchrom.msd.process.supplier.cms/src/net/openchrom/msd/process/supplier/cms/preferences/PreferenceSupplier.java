ed7eefdc3f0d0d5e30d131d53c54e101759882e7
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-12-01 11:44:25 +0100

    Initial project setup.

---
+++ openchrom/plugins/net.openchrom.msd.process.supplier.cms/src/net/openchrom/msd/process/supplier/cms/preferences/PreferenceSupplier.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Walter Whitlock, Philip Wenig.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Walter Whitlock - initial API and implementation
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.msd.process.supplier.cms.preferences;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.chemclipse.support.preferences.IPreferenceSupplier;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.IScopeContext;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+
+import net.openchrom.msd.process.supplier.cms.Activator;
+
+public class PreferenceSupplier implements IPreferenceSupplier {
+
+	public static final String P_TEST = "test";
+	public static final String DEF_TEST = "";
+	//
+	private static IPreferenceSupplier preferenceSupplier;
+
+	public static IPreferenceSupplier INSTANCE() {
+
+		if(preferenceSupplier == null) {
+			preferenceSupplier = new PreferenceSupplier();
+		}
+		return preferenceSupplier;
+	}
+
+	@Override
+	public IScopeContext getScopeContext() {
+
+		return InstanceScope.INSTANCE;
+	}
+
+	@Override
+	public String getPreferenceNode() {
+
+		return Activator.getContext().getBundle().getSymbolicName();
+	}
+
+	@Override
+	public Map<String, String> getDefaultValues() {
+
+		Map<String, String> defaultValues = new HashMap<String, String>();
+		//
+		defaultValues.put(P_TEST, DEF_TEST);
+		//
+		return defaultValues;
+	}
+
+	@Override
+	public IEclipsePreferences getPreferences() {
+
+		return getScopeContext().getNode(getPreferenceNode());
+	}
+
+	public static String getTest() {
+
+		IEclipsePreferences preferences = PreferenceSupplier.INSTANCE().getPreferences();
+		return preferences.get(P_TEST, DEF_TEST);
+	}
+}
