551274fff967ddfc5772c1a023a996fee682cadf
Philip Wenig <philip.wenig@openchrom.net>
2016-12-23 18:02:10 +0100

    The header info has been updated.

--- openchrom/plugins/net.openchrom.chromatogram.msd.comparison.supplier.pbm/src/net/openchrom/chromatogram/msd/comparison/supplier/pbm/comparator/MassSpectrumComparator.java
+++ openchrom/plugins/net.openchrom.chromatogram.msd.comparison.supplier.pbm/src/net/openchrom/chromatogram/msd/comparison/supplier/pbm/comparator/MassSpectrumComparator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2016 Philip (eselmeister) Wenig.
+ * Copyright (c) 2008, 2016 Dr. Philip Wenig.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
@@ -7,7 +7,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * Philip (eselmeister) Wenig - initial API and implementation
+ * Dr. Philip Wenig - initial API and implementation
  *******************************************************************************/
 package net.openchrom.chromatogram.msd.comparison.supplier.pbm.comparator;
 
