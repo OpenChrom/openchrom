a48a062fd5f8bbacab5c6a3e343d762afdd91396
Philip Wenig <philip.wenig@openchrom.net>
2016-12-27 18:59:21 +0100

    The copyright headers have been cleaned up.

--- openchrom/plugins/net.openchrom.chromatogram.msd.comparison.supplier.pbm.ui/src/net/openchrom/chromatogram/msd/comparison/supplier/pbm/ui/Activator.java
+++ openchrom/plugins/net.openchrom.chromatogram.msd.comparison.supplier.pbm.ui/src/net/openchrom/chromatogram/msd/comparison/supplier/pbm/ui/Activator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2016 Dr. Philip Wenig.
+ * Copyright (c) 2008, 2016 Lablicate GmbH.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
