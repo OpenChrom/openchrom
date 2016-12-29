7db5c10a6e46e43759ccac4c682c8e892a6d0118
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-12-29 11:33:23 +0100

    The headers have been updated.

--- openchrom/plugins/net.openchrom.chromatogram.msd.comparison.supplier.pbm.fragment.test/src/net/openchrom/chromatogram/msd/comparison/supplier/pbm/comparator/PBMMassSpectrumComparator_4_Test.java
+++ openchrom/plugins/net.openchrom.chromatogram.msd.comparison.supplier.pbm.fragment.test/src/net/openchrom/chromatogram/msd/comparison/supplier/pbm/comparator/PBMMassSpectrumComparator_4_Test.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2016 Dr. Philip Wenig.
+ * Copyright (c) 2014, 2016 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
