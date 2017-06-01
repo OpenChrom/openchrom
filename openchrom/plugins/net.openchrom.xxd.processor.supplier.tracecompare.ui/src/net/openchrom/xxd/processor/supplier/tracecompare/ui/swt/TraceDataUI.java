40f2566ac487f291532806fd2ae897f4ad6b0fc0
Philip Wenig <philip.wenig@openchrom.net>
2017-06-01 10:55:57 +0200

    The settings have been modified.

--- openchrom/plugins/net.openchrom.xxd.processor.supplier.tracecompare.ui/src/net/openchrom/xxd/processor/supplier/tracecompare/ui/swt/TraceDataUI.java
+++ openchrom/plugins/net.openchrom.xxd.processor.supplier.tracecompare.ui/src/net/openchrom/xxd/processor/supplier/tracecompare/ui/swt/TraceDataUI.java
@@ -48,7 +48,7 @@ public class TraceDataUI extends LineChart implements IChart {
 
 		IChartSettings chartSettings = getChartSettings();
 		chartSettings.setOrientation(SWT.HORIZONTAL);
-		chartSettings.setRangeInfoVisible(true);
+		chartSettings.enableRangeInfo(true);
 		chartSettings.setHorizontalSliderVisible(true);
 		chartSettings.setVerticalSliderVisible(false);
 		chartSettings.setUseZeroX(true);
