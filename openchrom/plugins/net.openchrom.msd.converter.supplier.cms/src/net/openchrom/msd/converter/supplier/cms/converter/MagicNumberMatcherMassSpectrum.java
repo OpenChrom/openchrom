e39d781d1163cb4abdbd43772f23f42b6848cf03
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-10-20 15:11:50 +0200

    The mass spectrum converter magic number matcher have been added.

---
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.cms/src/net/openchrom/msd/converter/supplier/cms/converter/MagicNumberMatcherMassSpectrum.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.msd.converter.supplier.cms.converter;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.converter.core.AbstractMagicNumberMatcher;
+import org.eclipse.chemclipse.converter.core.IMagicNumberMatcher;
+
+public class MagicNumberMatcherMassSpectrum extends AbstractMagicNumberMatcher implements IMagicNumberMatcher {
+
+	@Override
+	public boolean checkFileFormat(File file) {
+
+		if(file.getName().endsWith(".cms")) {
+			return true;
+		}
+		return false;
+	}
+}
