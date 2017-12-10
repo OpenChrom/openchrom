c2c04f947c71dce8c9d3d11b48731c8b899265c1
Walter Whitlock <walterhw@users.noreply.github.com>
2017-12-10 13:49:35 -0500

    add test for valid string before converting to double
    prevents NumberFormatException

--- openchrom/plugins/net.openchrom.msd.process.supplier.cms.ui/src/net/openchrom/msd/process/supplier/cms/ui/parts/swt/CompositeCompositionsUI.java
+++ openchrom/plugins/net.openchrom.msd.process.supplier.cms.ui/src/net/openchrom/msd/process/supplier/cms/ui/parts/swt/CompositeCompositionsUI.java
@@ -15,6 +15,7 @@ package net.openchrom.msd.process.supplier.cms.ui.parts.swt;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.TreeMap;
+import java.util.regex.Pattern;
 
 import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.support.text.ValueFormat;
@@ -236,6 +237,43 @@ public class CompositeCompositionsUI extends Composite {
 		lightweightSystem.setContents(xyGraphComposition);
 	}
 
+	private boolean isValidDoubleString(String myString) {
+
+		final String Digits = "(\\p{Digit}+)";
+		final String HexDigits = "(\\p{XDigit}+)";
+		// an exponent is 'e' or 'E' followed by an optionally
+		// signed decimal integer.
+		final String Exp = "[eE][+-]?" + Digits;
+		final String fpRegex = ("[\\x00-\\x20]*" + // Optional leading "whitespace"
+				"[+-]?(" + // Optional sign character
+				"NaN|" + // "NaN" string
+				"Infinity|" + // "Infinity" string
+				// A decimal floating-point string representing a finite positive
+				// number without a leading sign has at most five basic pieces:
+				// Digits . Digits ExponentPart FloatTypeSuffix
+				//
+				// Since this method allows integer-only strings as input
+				// in addition to strings of floating-point literals, the
+				// two sub-patterns below are simplifications of the grammar
+				// productions from section 3.10.2 of
+				// The Java™ Language Specification.
+				// Digits ._opt Digits_opt ExponentPart_opt FloatTypeSuffix_opt
+				"(((" + Digits + "(\\.)?(" + Digits + "?)(" + Exp + ")?)|" +
+				// . Digits ExponentPart_opt FloatTypeSuffix_opt
+				"(\\.(" + Digits + ")(" + Exp + ")?)|" +
+				// Hexadecimal strings
+				"((" +
+				// 0[xX] HexDigits ._opt BinaryExponent FloatTypeSuffix_opt
+				"(0[xX]" + HexDigits + "(\\.)?)|" +
+				// 0[xX] HexDigits_opt . HexDigits BinaryExponent FloatTypeSuffix_opt
+				"(0[xX]" + HexDigits + "?(\\.)" + HexDigits + ")" + ")[pP][+-]?" + Digits + "))" + "[fFdD]?))" + "[\\x00-\\x20]*");// Optional trailing "whitespace"
+		if(Pattern.matches(fpRegex, myString))
+			return true; // Will not throw NumberFormatException
+		else {
+			return false;
+		}
+	}
+
 	private void clearXYGraph() {
 
 		if(null != traceCompositionsMap) {
@@ -361,8 +399,10 @@ public class CompositeCompositionsUI extends Composite {
 					textLogScaleOffset.setText(String.valueOf(scaleOffset));
 					txtLogScaleOffsetIgnoreEvent = false;
 				} else {
-					scaleOffset = Double.parseDouble(textLogScaleOffset.getText());
-					minAbsY = java.lang.StrictMath.abs(scaleOffset + minY); // compute new minAbsY for Y-axis limits
+					if(isValidDoubleString(textLogScaleOffset.getText())) {
+						scaleOffset = Double.parseDouble(textLogScaleOffset.getText());
+						minAbsY = java.lang.StrictMath.abs(scaleOffset + minY); // compute new minAbsY for Y-axis limits
+					}
 				}
 			}
 			for(String strName : lookup.keySet()) {
