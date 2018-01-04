e9171ee0da6e50a8dcbfccb65a3c85733ee011fa
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-01-04 11:02:18 +0100

    The copyright headers have been updated to 2018.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.cms/src/net/openchrom/msd/converter/supplier/cms/model/ICalibratedVendorLibraryMassSpectrum.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.cms/src/net/openchrom/msd/converter/supplier/cms/model/ICalibratedVendorLibraryMassSpectrum.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2017 Walter Whitlock, Philip Wenig.
+ * Copyright (c) 2016, 2018 Walter Whitlock, Philip Wenig.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse License v1.0
