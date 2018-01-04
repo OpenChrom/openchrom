0ab8ad538604154b28f9f71d46c9405c8ba6f814
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-01-04 11:02:20 +0100

    The copyright headers have been updated to 2018.

--- openchrom/plugins/net.openchrom.wsd.converter.supplier.abif.fragment.test/src/net/openchrom/wsd/converter/supplier/abif/io/EmptyFile_ITest.java
+++ openchrom/plugins/net.openchrom.wsd.converter.supplier.abif.fragment.test/src/net/openchrom/wsd/converter/supplier/abif/io/EmptyFile_ITest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2017 Matthias Mailänder.
+ * Copyright (c) 2016, 2018 Matthias Mailänder.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
