427253aa2fb689b3909b01bc1fa003a5ce11b8ea
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-01-05 17:55:01 +0100

    The copyright header has been updated.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.mgf/src/net/openchrom/msd/converter/supplier/mgf/converter/MagicNumberMatcherMassSpectrum.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mgf/src/net/openchrom/msd/converter/supplier/mgf/converter/MagicNumberMatcherMassSpectrum.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 Lablicate GmbH.
+ * Copyright (c) 2017, 2018 Lablicate GmbH.
  *
  * All rights reserved.
  * This program and the accompanying materials are made available under the
