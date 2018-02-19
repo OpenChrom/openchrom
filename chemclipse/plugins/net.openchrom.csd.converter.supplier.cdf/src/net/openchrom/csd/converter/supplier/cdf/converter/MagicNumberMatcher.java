65bd2713954ef20df5b5e60db3f956d69793539e
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-02-19 17:17:49 +0100

    DEVELOP-306 - the magic number matcher has been improved.

--- chemclipse/plugins/net.openchrom.csd.converter.supplier.cdf/src/net/openchrom/csd/converter/supplier/cdf/converter/MagicNumberMatcher.java
+++ chemclipse/plugins/net.openchrom.csd.converter.supplier.cdf/src/net/openchrom/csd/converter/supplier/cdf/converter/MagicNumberMatcher.java
@@ -12,35 +12,51 @@
 package net.openchrom.csd.converter.supplier.cdf.converter;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.eclipse.chemclipse.converter.core.AbstractMagicNumberMatcher;
 import org.eclipse.chemclipse.converter.core.IMagicNumberMatcher;
-import org.eclipse.chemclipse.csd.converter.io.IChromatogramCSDReader;
-import org.eclipse.chemclipse.model.core.IChromatogramOverview;
-import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.chemclipse.logging.core.Logger;
 
-import net.openchrom.csd.converter.supplier.cdf.internal.converter.SpecificationValidator;
-import net.openchrom.csd.converter.supplier.cdf.io.ChromatogramReader;
+import ucar.nc2.NetcdfFile;
 
 public class MagicNumberMatcher extends AbstractMagicNumberMatcher implements IMagicNumberMatcher {
+
+	private static final Logger logger = Logger.getLogger(MagicNumberMatcher.class);
+	public static final String VARIABLE_MASS_VALUES = "mass_values";
 
 	@Override
 	public boolean checkFileFormat(File file) {
 
 		boolean isValidFormat = false;
-		try {
+		if(file != null) {
 			String fileName = file.getName().toLowerCase();
-			if(fileName.endsWith("cdf")) {
-				file = SpecificationValidator.validateSpecification(file);
-				IChromatogramCSDReader reader = new ChromatogramReader();
-				IChromatogramOverview chromatogramOverview = reader.readOverview(file, new NullProgressMonitor());
-				if(chromatogramOverview != null) {
-					isValidFormat = true;
+			if(fileName.endsWith(".cdf")) {
+				NetcdfFile ncfile = null;
+				try {
+					/*
+					 * If no mass values are stored, assume that it is a FID file.
+					 */
+					ncfile = NetcdfFile.open(file.getAbsolutePath());
+					if(ncfile != null) {
+						if(ncfile.findVariable(VARIABLE_MASS_VALUES) == null) {
+							isValidFormat = true;
+						}
+					}
+				} catch(IOException e) {
+					logger.warn(e);
+				} finally {
+					if(ncfile != null) {
+						try {
+							ncfile.close();
+						} catch(IOException e) {
+							logger.warn(e);
+						}
+					}
 				}
 			}
-		} catch(Exception e) {
-			// Print no exception.
 		}
+		//
 		return isValidFormat;
 	}
 }
