0a7604c28c1d7ef0a4d58ad0d07ec6b1c6a7b05c
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-06-08 12:43:12 +0200

    The test case has been fixed.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.cms.fragment.test/src/net/openchrom/msd/converter/supplier/cms/TestPathHelper.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.cms.fragment.test/src/net/openchrom/msd/converter/supplier/cms/TestPathHelper.java
@@ -18,7 +18,7 @@ package net.openchrom.msd.converter.supplier.cms;
  */
 public class TestPathHelper extends PathResolver {
 
-	public static final String TESTFILE_IMPORT_MASS_SPECTRA_1 = "testData/files/import/MassSpectra1.CMS";
+	public static final String TESTFILE_IMPORT_MASS_SPECTRA_1 = "testData/files/import/MassSpectra1.cms";
 	public static final String TESTFILE_IMPORT_MASS_SPECTRA_2 = "testData/files/import/MassSpectra2.cms";
 	public static final String TESTFILE_IMPORT_MASS_SPECTRA_3 = "testData/files/import/MassSpectra3.cms";
 	public static final String TESTFILE_IMPORT_MASS_SPECTRA_4 = "testData/files/import/MassSpectra4.cms";
