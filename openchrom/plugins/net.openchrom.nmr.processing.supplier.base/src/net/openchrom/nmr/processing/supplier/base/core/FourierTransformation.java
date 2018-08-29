d5201ca824c4884b27455303357c42c3a8c99044
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-08-29 13:37:35 +0200

    Initial project setup.

---
+++ openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/supplier/base/core/FourierTransformation.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.nmr.processing.supplier.base.core;
+
+import org.apache.commons.math3.complex.Complex;
+import org.eclipse.chemclipse.nmr.model.core.IScanNMR;
+import org.eclipse.chemclipse.nmr.processor.core.AbstractScanProcessor;
+import org.eclipse.chemclipse.nmr.processor.core.IScanProcessor;
+import org.eclipse.chemclipse.nmr.processor.settings.IProcessorSettings;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import net.openchrom.nmr.processing.supplier.base.settings.FourierTransformationSettings;
+
+public class FourierTransformation extends AbstractScanProcessor implements IScanProcessor {
+
+	@Override
+	public IProcessingInfo process(IScanNMR scanNMR, IProcessorSettings processorSettings, IProgressMonitor monitor) {
+
+		IProcessingInfo processingInfo = validate(scanNMR, processorSettings, FourierTransformationSettings.class);
+		if(!processingInfo.hasErrorMessages()) {
+			FourierTransformationSettings settings = (FourierTransformationSettings)processorSettings;
+			Complex[] modifiedSignals = scanNMR.getModifiedSignals();
+			/*
+			 * Add your implementation here.
+			 */
+		}
+		return processingInfo;
+	}
+}
