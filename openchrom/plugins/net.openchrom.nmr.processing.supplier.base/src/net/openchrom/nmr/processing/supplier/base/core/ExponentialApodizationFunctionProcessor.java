3c765cc7c24fc630ca8a153423a92c4c245d4581
Jan Holy <jan.holy91@gmail.com>
2018-11-19 16:35:21 +0100

    create empty class for Apodization Function

---
+++ openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/supplier/base/core/ExponentialApodizationFunctionProcessor.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * jan - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.nmr.processing.supplier.base.core;
+
+import org.eclipse.chemclipse.nmr.model.core.IScanNMR;
+import org.eclipse.chemclipse.nmr.processor.core.AbstractScanProcessor;
+import org.eclipse.chemclipse.nmr.processor.core.IScanProcessor;
+import org.eclipse.chemclipse.nmr.processor.settings.IProcessorSettings;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ExponentialApodizationFunctionProcessor extends AbstractScanProcessor implements IScanProcessor {
+
+	public ExponentialApodizationFunctionProcessor() {
+
+	}
+
+	@Override
+	public IProcessingInfo process(IScanNMR scanNMR, IProcessorSettings processorSettings, IProgressMonitor monitor) {
+
+		return null;
+	}
+}
