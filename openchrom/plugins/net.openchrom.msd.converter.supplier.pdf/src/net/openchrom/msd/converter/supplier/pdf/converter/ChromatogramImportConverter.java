36ca0e806ba1ab5951fed4668fd39b778411f9b5
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-12-12 14:54:03 +0100

    The chromatogram converter has been refactored.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.pdf/src/net/openchrom/msd/converter/supplier/pdf/converter/ChromatogramImportConverter.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.pdf/src/net/openchrom/msd/converter/supplier/pdf/converter/ChromatogramImportConverter.java
@@ -13,13 +13,13 @@ package net.openchrom.msd.converter.supplier.pdf.converter;
 
 import java.io.File;
 
+import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramImportConverter;
 import org.eclipse.chemclipse.converter.chromatogram.IChromatogramImportConverter;
-import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDImportConverter;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.chemclipse.processing.core.ProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-public class ChromatogramImportConverter extends AbstractChromatogramMSDImportConverter implements IChromatogramImportConverter {
+public class ChromatogramImportConverter extends AbstractChromatogramImportConverter implements IChromatogramImportConverter {
 
 	private static final String DESCRIPTION = "PDF Import Converter";
 	private static final String EXPORT_ERROR = "The PDF converter is not able to import chromatograms.";
