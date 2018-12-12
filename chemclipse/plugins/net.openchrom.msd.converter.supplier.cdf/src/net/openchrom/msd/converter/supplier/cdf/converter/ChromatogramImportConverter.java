a69c1d85351b9d68c968450ee6d5e9539709f883
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-12-12 14:54:02 +0100

    The chromatogram converter has been refactored.

--- chemclipse/plugins/net.openchrom.msd.converter.supplier.cdf/src/net/openchrom/msd/converter/supplier/cdf/converter/ChromatogramImportConverter.java
+++ chemclipse/plugins/net.openchrom.msd.converter.supplier.cdf/src/net/openchrom/msd/converter/supplier/cdf/converter/ChromatogramImportConverter.java
@@ -13,10 +13,10 @@ package net.openchrom.msd.converter.supplier.cdf.converter;
 
 import java.io.File;
 
+import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramImportConverter;
 import org.eclipse.chemclipse.converter.chromatogram.IChromatogramImportConverter;
 import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.model.core.IChromatogramOverview;
-import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDImportConverter;
 import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDReader;
 import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
@@ -26,7 +26,7 @@ import net.openchrom.msd.converter.supplier.cdf.internal.converter.Specification
 import net.openchrom.msd.converter.supplier.cdf.internal.support.IConstants;
 import net.openchrom.msd.converter.supplier.cdf.io.ChromatogramReader;
 
-public class ChromatogramImportConverter extends AbstractChromatogramMSDImportConverter implements IChromatogramImportConverter {
+public class ChromatogramImportConverter extends AbstractChromatogramImportConverter implements IChromatogramImportConverter {
 
 	private static final Logger logger = Logger.getLogger(ChromatogramImportConverter.class);
 	private static final String DESCRIPTION = "NetCDF Import Converter";
