4b01d8b24ff0154c72b1473b067ceb1e02ec3c51
Dr. Philip Wenig <philip.wenig@openchrom.net>
2018-12-13 07:54:13 +0100

    The chromatogram converter handling has been improved.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.pdf/src/net/openchrom/msd/converter/supplier/pdf/converter/ChromatogramExportConverter.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.pdf/src/net/openchrom/msd/converter/supplier/pdf/converter/ChromatogramExportConverter.java
@@ -13,9 +13,11 @@ package net.openchrom.msd.converter.supplier.pdf.converter;
 
 import java.io.File;
 
+import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramExportConverter;
 import org.eclipse.chemclipse.converter.chromatogram.IChromatogramExportConverter;
 import org.eclipse.chemclipse.logging.core.Logger;
-import org.eclipse.chemclipse.msd.converter.chromatogram.AbstractChromatogramMSDExportConverter;
+import org.eclipse.chemclipse.model.core.IChromatogram;
+import org.eclipse.chemclipse.model.core.IPeak;
 import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -23,20 +25,21 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import net.openchrom.msd.converter.supplier.pdf.internal.converter.SpecificationValidator;
 import net.openchrom.msd.converter.supplier.pdf.io.ChromatogramWriter;
 
-public class ChromatogramExportConverter extends AbstractChromatogramMSDExportConverter implements IChromatogramExportConverter {
+public class ChromatogramExportConverter extends AbstractChromatogramExportConverter implements IChromatogramExportConverter {
 
 	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
 	private static final String DESCRIPTION = "PDF Export Converter";
 
 	@Override
-	public IProcessingInfo convert(File file, IChromatogramMSD chromatogram, IProgressMonitor monitor) {
+	public IProcessingInfo convert(File file, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
 
 		file = SpecificationValidator.validateSpecification(file);
 		IProcessingInfo processingInfo = super.validate(file);
-		if(!processingInfo.hasErrorMessages()) {
+		if(!processingInfo.hasErrorMessages() && chromatogram instanceof IChromatogramMSD) {
+			IChromatogramMSD chromatogramMSD = (IChromatogramMSD)chromatogram;
 			ChromatogramWriter writer = new ChromatogramWriter();
 			try {
-				writer.writeChromatogram(file, chromatogram, monitor);
+				writer.writeChromatogram(file, chromatogramMSD, monitor);
 				processingInfo.setProcessingResult(file);
 			} catch(Exception e) {
 				logger.warn(e);
