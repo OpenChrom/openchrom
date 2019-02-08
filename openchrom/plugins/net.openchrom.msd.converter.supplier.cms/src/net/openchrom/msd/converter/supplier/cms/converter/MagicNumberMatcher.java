9aa7d9adcdf03da5b481e6aa02fb709336424d3b
Dr. Philip Wenig <philip.wenig@openchrom.net>
2019-02-08 18:10:36 +0100

    DEVELOP-1013 - Optimized the magic number checker

--- openchrom/plugins/net.openchrom.msd.converter.supplier.cms/src/net/openchrom/msd/converter/supplier/cms/converter/MagicNumberMatcher.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.cms/src/net/openchrom/msd/converter/supplier/cms/converter/MagicNumberMatcher.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2018 Lablicate GmbH.
+ * Copyright (c) 2017, 2019 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -16,14 +16,11 @@ import java.io.File;
 import org.eclipse.chemclipse.converter.core.AbstractMagicNumberMatcher;
 import org.eclipse.chemclipse.converter.core.IMagicNumberMatcher;
 
-public class MagicNumberMatcherDatabase extends AbstractMagicNumberMatcher implements IMagicNumberMatcher {
+public class MagicNumberMatcher extends AbstractMagicNumberMatcher implements IMagicNumberMatcher {
 
 	@Override
 	public boolean checkFileFormat(File file) {
 
-		if(file.getName().endsWith(".cms")) {
-			return true;
-		}
-		return false;
+		return checkFileExtension(file, ".cms");
 	}
 }
