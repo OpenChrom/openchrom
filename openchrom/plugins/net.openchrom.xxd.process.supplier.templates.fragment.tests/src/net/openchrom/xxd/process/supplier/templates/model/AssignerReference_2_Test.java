5e2928416384d0acdbec8b15c0b4d704c961af9e
Dr. Philip Wenig <philip.wenig@openchrom.net>
2019-02-14 07:45:09 +0100

    Additional test cases have been added.

---
+++ openchrom/plugins/net.openchrom.xxd.process.supplier.templates.fragment.tests/src/net/openchrom/xxd/process/supplier/templates/model/AssignerReference_2_Test.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.xxd.process.supplier.templates.model;
+
+import junit.framework.TestCase;
+
+public class AssignerReference_2_Test extends TestCase {
+
+	private AssignerReference setting;
+
+	@Override
+	protected void setUp() throws Exception {
+
+		super.setUp();
+		setting = new AssignerReference();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+
+		super.tearDown();
+	}
+
+	public void test1() {
+
+		setting.setName("Hello");
+		assertEquals("Hello", setting.getName());
+	}
+
+	public void test2() {
+
+		setting.setStartRetentionTime(3.45d);
+		assertEquals(3.45d, setting.getStartRetentionTime());
+	}
+
+	public void test3() {
+
+		setting.setStopRetentionTime(10.98d);
+		assertEquals(10.98d, setting.getStopRetentionTime());
+	}
+
+	public void test4() {
+
+		setting.setIdentifier("World");
+		assertEquals("World", setting.getIdentifier());
+	}
+}
---
+++ openchrom/plugins/net.openchrom.xxd.process.supplier.templates.fragment.tests/src/net/openchrom/xxd/process/supplier/templates/model/IdentifierSetting_2_Test.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.xxd.process.supplier.templates.model;
+
+import junit.framework.TestCase;
+
+public class IdentifierSetting_2_Test extends TestCase {
+
+	private IdentifierSetting setting;
+
+	@Override
+	protected void setUp() throws Exception {
+
+		super.setUp();
+		setting = new IdentifierSetting();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+
+		super.tearDown();
+	}
+
+	public void test1() {
+
+		setting.setStartRetentionTime(1.23d);
+		assertEquals(1.23d, setting.getStartRetentionTime());
+	}
+
+	public void test2() {
+
+		setting.setStopRetentionTime(6.63d);
+		assertEquals(6.63d, setting.getStopRetentionTime());
+	}
+
+	public void test3() {
+
+		setting.setName("Styrene");
+		assertEquals("Styrene", setting.getName());
+	}
+
+	public void test4() {
+
+		setting.setCasNumber("100-42-5");
+		assertEquals("100-42-5", setting.getCasNumber());
+	}
+
+	public void test5() {
+
+		setting.setComments("Test");
+		assertEquals("Test", setting.getComments());
+	}
+
+	public void test6() {
+
+		setting.setContributor("OpenChrom");
+		assertEquals("OpenChrom", setting.getContributor());
+	}
+
+	public void test7() {
+
+		setting.setReferenceId("REF-7");
+		assertEquals("REF-7", setting.getReferenceId());
+	}
+
+	public void test8() {
+
+		setting.setTraces("103, 104");
+		assertEquals("103, 104", setting.getTraces());
+	}
+}
