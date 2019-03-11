307e4c3d02bf86a8dc64a64c0e6c2f863d04982f
kerner1000 <alexkerner1000@gmail.com>
2019-03-11 18:46:06 +0100

    [fix] DEVELOP-1026

--- openchrom/plugins/net.openchrom.chromatogram.msd.comparison.supplier.pbm.fragment.test/src/net/openchrom/chromatogram/msd/comparison/supplier/pbm/comparator/PBMMassSpectrumComparator_6_Test.java
+++ openchrom/plugins/net.openchrom.chromatogram.msd.comparison.supplier.pbm.fragment.test/src/net/openchrom/chromatogram/msd/comparison/supplier/pbm/comparator/PBMMassSpectrumComparator_6_Test.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2018 Lablicate GmbH.
- * 
+ * Copyright (c) 2014, 2019 Lablicate GmbH.
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Alexander Kerner - Generics
  *******************************************************************************/
 package net.openchrom.chromatogram.msd.comparison.supplier.pbm.comparator;
 
@@ -18,7 +19,7 @@ import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 public class PBMMassSpectrumComparator_6_Test extends MassSpectrumSetTestCase {
 
 	private MassSpectrumComparator comparator;
-	private IProcessingInfo processingInfo;
+	private IProcessingInfo<IComparisonResult> processingInfo;
 	private IComparisonResult result;
 
 	@Override
