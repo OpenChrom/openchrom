7187cd6c2416e4cdba95778d4ba47fe809b74b9d
Alexander Kerner <alexanderkerner24@gmail.com>
2019-05-01 18:59:45 +0200

    Implement io.Serializable

--- openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/supplier/base/core/ComplexSpectrumSignal.java
+++ openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/supplier/base/core/ComplexSpectrumSignal.java
@@ -5,17 +5,23 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Christoph Läubrich - initial API and implementation
  *******************************************************************************/
 package net.openchrom.nmr.processing.supplier.base.core;
 
+import java.io.Serializable;
+
 import org.apache.commons.math3.complex.Complex;
 import org.eclipse.chemclipse.nmr.model.core.SpectrumSignal;
 
-public final class ComplexSpectrumSignal implements SpectrumSignal {
+public final class ComplexSpectrumSignal implements SpectrumSignal, Serializable {
 
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -3361359307799587392L;
 	private Complex complex;
 	private Number shift;
 
