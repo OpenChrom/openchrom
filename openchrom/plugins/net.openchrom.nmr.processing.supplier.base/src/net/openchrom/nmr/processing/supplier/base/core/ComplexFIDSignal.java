5e6620b97ee7886a38892cee098d403f023acbce
Alexander Kerner <alexanderkerner24@gmail.com>
2019-05-02 18:23:05 +0200

    Re-generate serialVersionUID

--- openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/supplier/base/core/ComplexFIDSignal.java
+++ openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/supplier/base/core/ComplexFIDSignal.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Alexander Stark - initial API and implementation
+ * Alexander Kerner - serialization
+ *******************************************************************************/
 package net.openchrom.nmr.processing.supplier.base.core;
 
 import java.io.Serializable;
@@ -8,7 +20,10 @@ import org.eclipse.chemclipse.nmr.model.core.FIDSignal;
 
 public class ComplexFIDSignal implements FIDSignal, Serializable {
 
-	private static final long serialVersionUID = -561512031857962638L;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1324819102443373298L;
 	private Complex complex;
 	private BigDecimal time;
 
