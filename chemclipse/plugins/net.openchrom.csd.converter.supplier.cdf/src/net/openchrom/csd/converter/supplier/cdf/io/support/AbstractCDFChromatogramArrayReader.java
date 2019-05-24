8fdb5849d6c574bf51caadd4cf1364b9b6a73c73
Dr. Philip Wenig <philip.wenig@openchrom.net>
2019-05-24 20:02:01 +0200

    Some Varian FID files store units as "time in minutes".

--- chemclipse/plugins/net.openchrom.csd.converter.supplier.cdf/src/net/openchrom/csd/converter/supplier/cdf/io/support/AbstractCDFChromatogramArrayReader.java
+++ chemclipse/plugins/net.openchrom.csd.converter.supplier.cdf/src/net/openchrom/csd/converter/supplier/cdf/io/support/AbstractCDFChromatogramArrayReader.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2018 Lablicate GmbH.
+ * Copyright (c) 2013, 2019 Lablicate GmbH.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
@@ -81,7 +81,7 @@ public abstract class AbstractCDFChromatogramArrayReader implements IAbstractCDF
 			String unit = retentionUnit.getStringValue().trim();
 			if(unit.equals("seconds") || unit.equals("Seconds")) {
 				retentionTimeScaleFactor = 1000;
-			} else if(unit.equals("minutes") || unit.equals("Minutes")) {
+			} else if(unit.equals("minutes") || unit.equals("Minutes") || unit.equals("time in minutes")) {
 				retentionTimeScaleFactor = 1000 * 60;
 			} else {
 				/*
