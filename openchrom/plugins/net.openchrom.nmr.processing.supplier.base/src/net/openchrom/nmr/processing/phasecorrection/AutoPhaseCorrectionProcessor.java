abf371e5e917b06b933b4c073c506363a644a5de
alexstark4 <39908870+alexstark4@users.noreply.github.com>
2019-06-02 09:27:42 +0200

    Automatic phase corr (#16)
    * DEVELOP-1149 Improve the automatic phase correction processor
    
    * An option has been added to "cut" left and right edges of spectrum
      and ignore this parts of spectrum on each side because they are often
      distorted.
    * A possibility has been added to correct the zeroth phase value only.
      It may sometimes be advisable to optimise only one value instead of
      optimising both (zeroth and first order).
    * Corresponding options have been added to the settings class to handle
      the new possibilities.
    * Minor re-factoring of some code
    
    * DEVELOP-1149 Introduce minor corrections
    
    * DEVELOP-1149 Implement further requested PR changes
    
    * substitute arrays with settings where appropriate
    * introduce default values for some parameters
    * change seed to Random
    
    * DEVELOP-1152 Implement a generic approach for storing value(s) (pairs)
    
    * DEVELOP-1152 Implement minor changes requested in PR (renaming)

--- openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/phasecorrection/AutoPhaseCorrectionProcessor.java
+++ openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/phasecorrection/AutoPhaseCorrectionProcessor.java
@@ -5,7 +5,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * Alexander Stark - initial API and implementation
  * Christoph Läubrich - rework for new filter model
@@ -13,6 +13,7 @@
 package net.openchrom.nmr.processing.phasecorrection;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import org.apache.commons.math3.analysis.MultivariateFunction;
 import org.apache.commons.math3.complex.Complex;
@@ -46,9 +47,38 @@ import net.openchrom.nmr.processing.supplier.base.settings.AutoPhaseCorrectionSe
 @Component(service = {Filter.class, IMeasurementFilter.class})
 public class AutoPhaseCorrectionProcessor extends AbstractSpectrumSignalFilter<AutoPhaseCorrectionSettings> {
 
+	private static final long serialVersionUID = 5088365386062088062L;
 	private static final String NAME = "Auto Phase Correction";
+	//
+	static final int DEFAULT_MAXIMUM_EVALUATIONS = 2000;
+	private static int maximumEvaluations = DEFAULT_MAXIMUM_EVALUATIONS;
+	static final int DEFAULT_MAXIMUM_ITERATIONS = 2000;
+	private static int maximumIterations = DEFAULT_MAXIMUM_ITERATIONS;
+	//
+	static AutoPhaseCorrectionProcessor aPCP = new AutoPhaseCorrectionProcessor();
+
+	public static int getMaxEvaluations() {
+
+		return maximumEvaluations;
+	}
+
+	public static void setMaxEvaluations(int maximumEvaluations) {
+
+		AutoPhaseCorrectionProcessor.maximumEvaluations = maximumEvaluations;
+	}
+
+	public static int getMaxIterations() {
+
+		return maximumIterations;
+	}
+
+	public static void setMaxIterations(int maximumIterations) {
+
+		AutoPhaseCorrectionProcessor.maximumIterations = maximumIterations;
+	}
 
 	public AutoPhaseCorrectionProcessor() {
+
 		super(AutoPhaseCorrectionSettings.class);
 	}
 
@@ -56,6 +86,51 @@ public class AutoPhaseCorrectionProcessor extends AbstractSpectrumSignalFilter<A
 	public String getFilterName() {
 
 		return NAME;
+	}
+
+	public final class PhaseCorrectionValue<T extends Number> {
+
+		private T zerothOrderValue;
+		private T firstOrderValue;
+
+		public PhaseCorrectionValue(T zerothOrderValue) {
+
+			this.zerothOrderValue = zerothOrderValue;
+		}
+
+		public PhaseCorrectionValue(T zerothOrderValue, T firstOrderValue) {
+
+			this.zerothOrderValue = zerothOrderValue;
+			this.firstOrderValue = firstOrderValue;
+		}
+
+		public T getZerothOrderValue() {
+
+			return zerothOrderValue;
+		}
+
+		public T getFirstOrderValue() {
+
+			return firstOrderValue;
+		}
+
+		public int getLengthOfValues() {
+
+			if(this.firstOrderValue == null) {
+				return 1;
+			} else {
+				return 2;
+			}
+		}
+
+		public double[] toArray() {
+
+			if(this.firstOrderValue == null) {
+				return new double[]{this.zerothOrderValue.doubleValue()};
+			} else {
+				return new double[]{this.zerothOrderValue.doubleValue(), this.firstOrderValue.doubleValue()};
+			}
+		}
 	}
 
 	@Override
@@ -72,49 +147,71 @@ public class AutoPhaseCorrectionProcessor extends AbstractSpectrumSignalFilter<A
 
 		Complex[] fourierTransformedSignals = spectrumData.signals;
 		//
-		//
 		/*
 		 * ACME algorithm - Chen et al., J Magn Res 2002, 158 (1), 164–168.
 		 */
 		// parts of optimizer
-		CalculateACMEEntropy calculateACMEEntropyFcn = new CalculateACMEEntropy(fourierTransformedSignals, settings.getPenaltyFactor());
+		CalculateACMEEntropy calculateACMEEntropyFcn = new CalculateACMEEntropy(fourierTransformedSignals, settings);
 		SimplexOptimizer nestedSimplexOptimizer = new SimplexOptimizer(1e-10, 1e-30);
-		double[] initialGuessMultiStart = new double[]{0, 0}; // {rightPhaseChange, leftPhaseChange};
-		NelderMeadSimplex nelderMeadSimplex = new NelderMeadSimplex(initialGuessMultiStart.length);
-		// NelderMeadSimplex nelderMeadSimplex = new NelderMeadSimplex(new double[] { 1, 5 }); // Steps along the canonical axes representing box edges
+		//
+		// settings.setCorrectOnlyZerothPhase(true);
+		PhaseCorrectionValue<Double> initialGuessValues;
+		if(settings.isCorrectOnlyZerothPhase()) {
+			// optimize only PHC0 value
+			initialGuessValues = aPCP.new PhaseCorrectionValue<Double>(0.0);
+		} else {
+			// optimize PHC0 and PHC1 values
+			initialGuessValues = aPCP.new PhaseCorrectionValue<Double>(0.0, 0.0);
+		}
+		NelderMeadSimplex nelderMeadSimplex = new NelderMeadSimplex(initialGuessValues.getLengthOfValues());
+		// Steps along the canonical axes representing box edges
+		// NelderMeadSimplex nelderMeadSimplex = new NelderMeadSimplex(new double[] { 1, 5 });
 		//
 		// generator for MultiStartMultivariateOptimizer
-		JDKRandomGenerator randomGenerator = new JDKRandomGenerator();
-		int seed = 1973582469;
-		randomGenerator.setSeed(seed);
-		int dimension = 2;
-		RandomVectorGenerator vectorGenerator = new UncorrelatedRandomVectorGenerator(dimension, new GaussianRandomGenerator(randomGenerator));
-		// optimizer
-		int numberOfRestarts = 3; // if 1 optimizer behaves like a MultivariateOptimizer
-		MultiStartMultivariateOptimizer multiStartPhaseOptimizer = new MultiStartMultivariateOptimizer(nestedSimplexOptimizer, numberOfRestarts, vectorGenerator);
-		PointValuePair optimizedPhaseValues = multiStartPhaseOptimizer.optimize(new MaxEval(2000), new MaxIter(2000), new ObjectiveFunction(calculateACMEEntropyFcn), GoalType.MINIMIZE, nelderMeadSimplex, new InitialGuess(initialGuessMultiStart));
-		// optimized values
-		double[] phaseOpt = new double[initialGuessMultiStart.length];
-		phaseOpt[0] = optimizedPhaseValues.getKey()[0];
-		phaseOpt[1] = optimizedPhaseValues.getKey()[1];
+		RandomVectorGenerator vectorGenerator = createRandomVectorGenerator(initialGuessValues.getLengthOfValues());
 		//
+		// optimizer
+		int numberOfRestarts = settings.getNumberOfOptimizationCycles(); // if 1 optimizer behaves like a MultivariateOptimizer
+		MultiStartMultivariateOptimizer multiStartPhaseOptimizer = //
+				new MultiStartMultivariateOptimizer(nestedSimplexOptimizer, numberOfRestarts, vectorGenerator);
+		PointValuePair optimizedPhaseValues = multiStartPhaseOptimizer.optimize(//
+				new MaxEval(getMaxEvaluations()), //
+				new MaxIter(getMaxIterations()), //
+				new ObjectiveFunction(calculateACMEEntropyFcn), //
+				GoalType.MINIMIZE, nelderMeadSimplex, //
+				new InitialGuess(initialGuessValues.toArray()));
+		// optimized values
+		if(settings.isCorrectOnlyZerothPhase()) {
+			// optimize only PHC0 value
+			settings.setPhaseCorrectionValues(aPCP.new PhaseCorrectionValue<Double>(optimizedPhaseValues.getKey()[0], 0.0));
+		} else {
+			// optimize PHC0 and PHC1 values
+			settings.setPhaseCorrectionValues(aPCP.new PhaseCorrectionValue<Double>(optimizedPhaseValues.getKey()[0], optimizedPhaseValues.getKey()[1]));
+		}
 		// phase spectrum - ps
-		Complex[] phaseCorrection = generatePhaseCorrection(fourierTransformedSignals.length, phaseOpt);
+		Complex[] phaseCorrection = generatePhaseCorrection(fourierTransformedSignals.length, settings);
 		for(int i = 0; i < phaseCorrection.length; i++) {
 			spectrumData.signals[i] = spectrumData.signals[i].multiply(phaseCorrection[i]);
 		}
 	}
 
+	private static RandomVectorGenerator createRandomVectorGenerator(int dimension) {
+
+		JDKRandomGenerator randomGenerator = new JDKRandomGenerator();
+		Random seed = new Random();
+		randomGenerator.setSeed(seed.nextInt());
+		return new UncorrelatedRandomVectorGenerator(dimension, new GaussianRandomGenerator(randomGenerator));
+	}
+
 	private static class CalculateACMEEntropy implements MultivariateFunction {
 
 		private final Complex[] fourierTransformedData;
-		private int iterCount;
-		private double penaltyFactor;
+		private final AutoPhaseCorrectionSettings settings;
 
-		public CalculateACMEEntropy(Complex[] fourierTransformedData, double penaltyFactor) {
+		public CalculateACMEEntropy(Complex[] fourierTransformedData, AutoPhaseCorrectionSettings settings) {
+
 			this.fourierTransformedData = fourierTransformedData;
-			this.penaltyFactor = penaltyFactor;
-			this.iterCount = 0;
+			this.settings = settings;
 		}
 
 		@Override
@@ -124,14 +221,32 @@ public class AutoPhaseCorrectionProcessor extends AbstractSpectrumSignalFilter<A
 			 */
 
 			int stepsize = 1;
-			++iterCount;
 			// (de)phase spectrum
 			int dataSize = fourierTransformedData.length;
-			Complex[] nmrDataComplex = applyPhaseCorrection(fourierTransformedData, parameters);
+			if(parameters.length == 1) {
+				settings.setPhaseCorrectionValues(aPCP.new PhaseCorrectionValue<Double>(parameters[0]));
+			} else {
+				settings.setPhaseCorrectionValues(aPCP.new PhaseCorrectionValue<Double>(parameters[0], parameters[1]));
+			}
+			Complex[] nmrDataComplex = applyPhaseCorrection(fourierTransformedData, settings);
 			double[] nmrDataReal = new double[dataSize];
 			for(int i = 0; i < dataSize; i++) {
 				nmrDataReal[i] = nmrDataComplex[i].getReal();
 			}
+			/*
+			 * "cut" left and right edges of spectrum (often distorted)
+			 */
+			double cutPercentage = settings.getOmitPercentOfTheSpectrum(); // ignore this % of spectrum each side
+			int cutPartOfSpectrum = (int)Math.round(nmrDataReal.length * cutPercentage * 0.01);
+			for(int i = 0; i <= cutPartOfSpectrum; i++) {
+				nmrDataReal[i] = 0;
+			}
+			//
+			int forInitialization = nmrDataReal.length - cutPartOfSpectrum + 1;
+			for(int i = forInitialization; i < nmrDataReal.length; i++) {
+				nmrDataReal[i] = 0;
+			}
+			//
 			// calculation of first derivatives
 			double[] derivativeArrayA = new double[dataSize - 2];
 			System.arraycopy(nmrDataReal, 2, derivativeArrayA, 0, derivativeArrayA.length);
@@ -176,37 +291,41 @@ public class AutoPhaseCorrectionProcessor extends AbstractSpectrumSignalFilter<A
 				double nmrDataRealAbsoluteSquare = Arrays.stream(tempSquare).sum();
 				pFun = pFun + nmrDataRealAbsoluteSquare / 4 / dataSize / dataSize;
 			}
-			double penaltyFunction = penaltyFactor * pFun;
-			iterCount++;
+			double penaltyFunction = settings.getPenaltyFactor() * pFun;
 			// value of objective function
 			double result = probabilityDistributionSum + penaltyFunction;
 			// System.out.format("%80.10f%n",result);
 			return result;
 		}
-
-		public int getCount() {
-
-			return iterCount;
-		}
 	}
 
-	private static Complex[] applyPhaseCorrection(Complex[] fourierTransformedData, double[] phaseParameters) {
+	private static Complex[] applyPhaseCorrection(Complex[] fourierTransformedData, AutoPhaseCorrectionSettings settings) {
 
 		int dataSize = fourierTransformedData.length;
-		Complex[] correction = generatePhaseCorrection(dataSize, phaseParameters);
+		Complex[] correction = generatePhaseCorrection(dataSize, settings);
 		Complex[] nmrDataIn = fourierTransformedData;
 		Complex[] nmrDataOut = new Complex[dataSize];
 		for(int i = 0; i < dataSize; i++) {
+			// apply correction
 			nmrDataOut[i] = nmrDataIn[i].multiply(correction[i]);
 		}
 		return nmrDataOut;
 	}
 
-	private static Complex[] generatePhaseCorrection(int dataSize, double[] phaseParameters) {
+	private static Complex[] generatePhaseCorrection(int dataSize, AutoPhaseCorrectionSettings settings) {
 
 		// convert to radians
-		double zerothPhase = phaseParameters[0] * Math.PI / 180;
-		double firstPhase = phaseParameters[1] * Math.PI / 180;
+		double zerothPhase = 0;
+		double firstPhase = 0;
+		if(settings.isCorrectOnlyZerothPhase()) {
+			// optimize only PHC0 value
+			zerothPhase = settings.getPhaseCorrectionValues().getZerothOrderValue() * Math.PI / 180;
+		} else {
+			// optimize PHC0 and PHC1 values
+			zerothPhase = settings.getPhaseCorrectionValues().getZerothOrderValue() * Math.PI / 180;
+			firstPhase = settings.getPhaseCorrectionValues().getFirstOrderValue() * Math.PI / 180;
+		}
+		//
 		Complex complexFactor = new Complex(0.0, 1.0); // sqrt(-1)
 		UtilityFunctions utilityFunction = new UtilityFunctions();
 		double[] phasingArray = utilityFunction.generateLinearlySpacedVector(0, dataSize, dataSize);
@@ -218,7 +337,6 @@ public class AutoPhaseCorrectionProcessor extends AbstractSpectrumSignalFilter<A
 		for(int i = 0; i < dataSize; i++) {
 			phaseCorrection[i] = phaseCorrection[i].exp();
 		}
-		// apply correction
 		return phaseCorrection;
 	}
 }
--- openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/supplier/base/settings/AutoPhaseCorrectionSettings.java
+++ openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/supplier/base/settings/AutoPhaseCorrectionSettings.java
@@ -1,11 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2018, 2019 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Alexander Stark - initial API and implementation
+ *******************************************************************************/
 package net.openchrom.nmr.processing.supplier.base.settings;
 
 import org.eclipse.chemclipse.nmr.processor.settings.IProcessorSettings;
 
+import net.openchrom.nmr.processing.phasecorrection.AutoPhaseCorrectionProcessor.PhaseCorrectionValue;
+
 public class AutoPhaseCorrectionSettings implements IProcessorSettings {
 
+	static final double DEFAULT_PENALTY_FACTORY = 1E-9 / 5;
+	static final int DEFAULT_NUMBER_OF_OPTIMIZATION_CYCLES = 1;
+	static final int DEFAULT_OMIT_PERCENT_OF_SPECTRUM = 15;
 	// user might provide better values
-	private double penaltyFactor = 1E-9 / 5;
+	private double penaltyFactor = DEFAULT_PENALTY_FACTORY;
+	private int numberOfOptimizationCycles = DEFAULT_NUMBER_OF_OPTIMIZATION_CYCLES;
+	private int omitPercentOfTheSpectrum = DEFAULT_OMIT_PERCENT_OF_SPECTRUM;
+	//
+	private boolean correctOnlyZerothPhase;
+	private PhaseCorrectionValue<Double> phaseCorrectionValues;
+
+	public PhaseCorrectionValue<Double> getPhaseCorrectionValues() {
+
+		return phaseCorrectionValues;
+	}
+
+	public void setPhaseCorrectionValues(PhaseCorrectionValue<Double> phaseCorrectionValues) {
+
+		this.phaseCorrectionValues = phaseCorrectionValues;
+	}
 
 	public double getPenaltyFactor() {
 
@@ -15,5 +46,35 @@ public class AutoPhaseCorrectionSettings implements IProcessorSettings {
 	public void setPenaltyFactor(double penaltyFactor) {
 
 		this.penaltyFactor = penaltyFactor;
+	}
+
+	public boolean isCorrectOnlyZerothPhase() {
+
+		return correctOnlyZerothPhase;
+	}
+
+	public void setCorrectOnlyZerothPhase(boolean correctOnlyZerothPhase) {
+
+		this.correctOnlyZerothPhase = correctOnlyZerothPhase;
+	}
+
+	public int getNumberOfOptimizationCycles() {
+
+		return numberOfOptimizationCycles;
+	}
+
+	public void setNumberOfOptimizationCycles(int numberOfOptimizationCycles) {
+
+		this.numberOfOptimizationCycles = numberOfOptimizationCycles;
+	}
+
+	public int getOmitPercentOfTheSpectrum() {
+
+		return omitPercentOfTheSpectrum;
+	}
+
+	public void setOmitPercentOfTheSpectrum(int omitPercentOfTheSpectrum) {
+
+		this.omitPercentOfTheSpectrum = omitPercentOfTheSpectrum;
 	}
 }
