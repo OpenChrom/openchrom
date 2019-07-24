686c9d3a2858b6be17e9d95ed3895a3548f7ec9b
Alexander Stark <alexander.stark@lablicate.com>
2019-07-24 14:46:23 +0200

    Change/simplify the removal of the digital filter
    After splitting the processor into three parts, the fundamental
    calculations for the removal of the digital filter have been replaced
    by a simpler "shifting" like approach.

--- openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/digitalfilter/DigitalFilterRemoval.java
+++ openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/digitalfilter/DigitalFilterRemoval.java
@@ -13,25 +13,27 @@
 package net.openchrom.nmr.processing.digitalfilter;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.math3.complex.Complex;
 import org.eclipse.chemclipse.model.core.FilteredMeasurement;
 import org.eclipse.chemclipse.model.filter.IMeasurementFilter;
-import org.eclipse.chemclipse.nmr.model.core.FilteredSpectrumMeasurement;
-import org.eclipse.chemclipse.nmr.model.core.SpectrumMeasurement;
+import org.eclipse.chemclipse.nmr.model.core.FIDMeasurement;
+import org.eclipse.chemclipse.nmr.model.core.FilteredFIDMeasurement;
 import org.eclipse.chemclipse.processing.core.MessageConsumer;
 import org.eclipse.chemclipse.processing.filter.Filter;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.osgi.service.component.annotations.Component;
 
-import net.openchrom.nmr.processing.supplier.base.core.AbstractSpectrumSignalFilter;
+import net.openchrom.nmr.processing.supplier.base.core.AbstractFIDSignalFilter;
 import net.openchrom.nmr.processing.supplier.base.core.UtilityFunctions;
-import net.openchrom.nmr.processing.supplier.base.core.UtilityFunctions.SpectrumData;
+import net.openchrom.nmr.processing.supplier.base.core.UtilityFunctions.ComplexFIDData;
 
 @Component(service = {Filter.class, IMeasurementFilter.class})
-public class DigitalFilterRemoval extends AbstractSpectrumSignalFilter<DigitalFilterRemovalSettings> implements Serializable {
+public class DigitalFilterRemoval extends AbstractFIDSignalFilter<DigitalFilterRemovalSettings> implements Serializable {
 
-	private static final long serialVersionUID = -3085947876861708195L;
+	private static final long serialVersionUID = -5870091035488215126L;
 	private static final String FILTER_NAME = "Digital Filter Removal";
 	private static final String MARKER = DigitalFilterRemoval.class.getName() + ".filtered";
 
@@ -41,7 +43,7 @@ public class DigitalFilterRemoval extends AbstractSpectrumSignalFilter<DigitalFi
 	}
 
 	@Override
-	protected boolean accepts(SpectrumMeasurement item) {
+	protected boolean accepts(FIDMeasurement item) {
 
 		return item.getHeaderData(MARKER) == null;
 	}
@@ -53,53 +55,42 @@ public class DigitalFilterRemoval extends AbstractSpectrumSignalFilter<DigitalFi
 	}
 
 	@Override
-	protected FilteredMeasurement<?> doFiltering(SpectrumMeasurement measurement, DigitalFilterRemovalSettings settings, MessageConsumer messageConsumer, IProgressMonitor monitor) {
+	protected FilteredMeasurement<?> doFiltering(FIDMeasurement measurement, DigitalFilterRemovalSettings settings, MessageConsumer messageConsumer, IProgressMonitor monitor) {
 
 		double multiplicationFactor = settings.getDcOffsetMultiplicationFactor();
-		double leftRotationFid = settings.getLeftRotationFid();
+		int leftRotationFid = (int)settings.getLeftRotationFid();
 		if(Double.isNaN(leftRotationFid) && Double.isNaN(multiplicationFactor)) {
 			messageConsumer.addInfoMessage(FILTER_NAME, "No Left Rotation value and no DC offset specified, skipp processing");
 			return null;
 		}
-		FilteredSpectrumMeasurement filteredMeasurement = new FilteredSpectrumMeasurement(measurement);
-		SpectrumData spectrumData = UtilityFunctions.toComplexSpectrumData(filteredMeasurement.getSignals());
+		FilteredFIDMeasurement filteredFIDMeasurement = new FilteredFIDMeasurement(measurement);
+		ComplexFIDData fidData = UtilityFunctions.toComplexFIDData(measurement.getSignals());
 		if(Double.isNaN(multiplicationFactor)) {
 			messageConsumer.addInfoMessage(FILTER_NAME, "No DC Offset to remove");
 		} else {
-			spectrumData.signals[0] = spectrumData.signals[0].multiply(multiplicationFactor);
+			fidData.signals[0] = fidData.signals[0].multiply(multiplicationFactor);
 			messageConsumer.addInfoMessage(FILTER_NAME, "DC Offset was removed");
 		}
 		if(Double.isNaN(leftRotationFid)) {
 			messageConsumer.addInfoMessage(FILTER_NAME, "No left rotation value was given, skipp processing");
 		} else if(Math.abs(leftRotationFid) > 0.0) {
-			DigitalFilterRemoval.removeDigitalFilter(spectrumData, leftRotationFid, messageConsumer);
+			DigitalFilterRemoval.removeDigitalFilter(fidData, leftRotationFid, messageConsumer);
 			messageConsumer.addInfoMessage(FILTER_NAME, "Digital FIlter was removed");
 		} else {
 			messageConsumer.addWarnMessage(FILTER_NAME, "Left Rotation value must be greater than zero, skipp processing");
 		}
-		filteredMeasurement.putHeaderData(MARKER, "true");
-		filteredMeasurement.setSignals(spectrumData.toSignal());
-		return filteredMeasurement;
+		filteredFIDMeasurement.putHeaderData(MARKER, "true");
+		filteredFIDMeasurement.setSignals(fidData.toSignal());
+		return filteredFIDMeasurement;
 	}
 
-	private static void removeDigitalFilter(SpectrumData spectrumData, double leftRotationFid, MessageConsumer messageConsumer) {
+	private static void removeDigitalFilter(ComplexFIDData fidData, int leftRotationFid, MessageConsumer messageConsumer) {
 
-		// create filtered spectrum
-		Complex[] unfilteredNMRSpectrum = new Complex[spectrumData.signals.length];
-		double digitalFilterFactor = 0;
-		int spectrumSize = spectrumData.signals.length;
-		Complex complexFactor = new Complex(-0.0, -1.0);
-		// remove the filter!
-		for(int i = 0; i < spectrumSize; i++) {
-			double filterTermA = (double)i / spectrumSize;
-			double filterTermB = Math.floor(spectrumSize / 2);
-			digitalFilterFactor = filterTermB - filterTermA;
-			Complex exponentialFactor = complexFactor.multiply(leftRotationFid * 2 * Math.PI * digitalFilterFactor);
-			unfilteredNMRSpectrum[i] = spectrumData.signals[i].multiply(exponentialFactor.exp());
-		}
-		// copy transformed data back to datastructure
-		for(int i = 0; i < spectrumData.signals.length; i++) {
-			spectrumData.signals[i] = unfilteredNMRSpectrum[i];
+		Complex[] digitalFilter = Arrays.copyOfRange(fidData.signals, 0, leftRotationFid);
+		Complex[] analogFID = Arrays.copyOfRange(fidData.signals, leftRotationFid, fidData.signals.length);
+		Complex[] unfilteredFID = ArrayUtils.addAll(analogFID, digitalFilter);
+		for(int i = 0; i < fidData.signals.length; i++) {
+			fidData.signals[i] = unfilteredFID[i];
 		}
 	}
 }
