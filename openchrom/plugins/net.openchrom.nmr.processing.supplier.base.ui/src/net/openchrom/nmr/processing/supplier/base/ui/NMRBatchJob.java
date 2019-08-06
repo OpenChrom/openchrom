2c23bc64a3a10621a079a14bc5e505d6ddc9cba1
Christoph Läubrich <christoph.laeubrich@lablicate.com>
2019-08-06 07:06:30 +0000

    Allow to open the BatchJob UI (not functional yet)

---
+++ openchrom/plugins/net.openchrom.nmr.processing.supplier.base.ui/src/net/openchrom/nmr/processing/supplier/base/ui/NMRBatchJob.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.nmr.processing.supplier.base.ui;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collections;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.chemclipse.model.methods.ProcessMethod;
+import org.eclipse.chemclipse.model.types.DataType;
+import org.eclipse.chemclipse.processing.filter.FilterFactory;
+import org.eclipse.chemclipse.ux.extension.xxd.ui.swt.BatchJobUI;
+import org.eclipse.chemclipse.xxd.process.support.ProcessTypeSupport;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.widgets.Composite;
+
+public class NMRBatchJob implements IRunnableWithProgress {
+
+	@Inject
+	private FilterFactory filterFactory;
+	private BatchJobUI batchJobUI;
+	private ProcessTypeSupport processTypeSupport;
+
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+
+		processTypeSupport = new ProcessTypeSupport(filterFactory);
+		batchJobUI = new BatchJobUI(parent, processTypeSupport, Activator.getDefault().getPreferenceStore(), "nmrBatchUIUserLocation", new DataType[]{DataType.NMR}, this);
+		batchJobUI.doLoad(Collections.emptyList(), new ProcessMethod());
+	}
+
+	@Override
+	public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+
+	}
+}
