79900de774b76f54ce5c886b8c77b01717f3e8b3
Christoph Läubrich <christoph.laeubrich@lablicate.com>
2019-08-14 12:20:32 +0000

    Fix Header

--- openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/supplier/base/settings/PhaseCorrectionSettings.java
+++ openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/supplier/base/settings/PhaseCorrectionSettings.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2018, 2019 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Alexander Kerner - initial API and implementation
+ * Jan Holy - refactoring
+ * Alexander Stark - refactoring
+ * Christoph Läubrich - refactoring
+ *******************************************************************************/
 package net.openchrom.nmr.processing.supplier.base.settings;
 
 import java.util.Observable;
