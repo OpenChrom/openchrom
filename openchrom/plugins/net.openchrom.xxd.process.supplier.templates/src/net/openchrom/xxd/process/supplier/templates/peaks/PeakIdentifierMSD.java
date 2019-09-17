1f7a1712f1086258d129a86e412d7c4a685c8537
Christoph Läubrich <christoph.laeubrich@lablicate.com>
2019-09-17 02:45:42 +0200

    Catch up with eclipse/chemclipse#9

--- openchrom/plugins/net.openchrom.xxd.process.supplier.templates/src/net/openchrom/xxd/process/supplier/templates/peaks/PeakIdentifierMSD.java
+++ openchrom/plugins/net.openchrom.xxd.process.supplier.templates/src/net/openchrom/xxd/process/supplier/templates/peaks/PeakIdentifierMSD.java
@@ -16,48 +16,21 @@ import java.util.List;
 
 import org.eclipse.chemclipse.chromatogram.msd.identifier.peak.IPeakIdentifierMSD;
 import org.eclipse.chemclipse.chromatogram.msd.identifier.settings.IPeakIdentifierSettingsMSD;
+import org.eclipse.chemclipse.model.identifier.IIdentificationResults;
 import org.eclipse.chemclipse.msd.model.core.IPeakMSD;
-import org.eclipse.chemclipse.msd.model.core.selection.IChromatogramSelectionMSD;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 import net.openchrom.xxd.process.supplier.templates.preferences.PreferenceSupplier;
 
-public class PeakIdentifierMSD<T> extends AbstractPeakIdentifier implements IPeakIdentifierMSD<T> {
+public class PeakIdentifierMSD<T> extends AbstractPeakIdentifier implements IPeakIdentifierMSD<IIdentificationResults> {
 
 	@Override
-	public IProcessingInfo<T> identify(List<? extends IPeakMSD> peaks, IPeakIdentifierSettingsMSD settings, IProgressMonitor monitor) {
+	public IProcessingInfo<IIdentificationResults> identify(List<? extends IPeakMSD> peaks, IPeakIdentifierSettingsMSD settings, IProgressMonitor monitor) {
 
+		if(settings == null) {
+			settings = getSettings(PreferenceSupplier.P_PEAK_IDENTIFIER_LIST_MSD);
+		}
 		return applyIdentifier(peaks, settings, monitor);
-	}
-
-	@Override
-	public IProcessingInfo<T> identify(IPeakMSD peak, IPeakIdentifierSettingsMSD settings, IProgressMonitor monitor) {
-
-		return identify(extractPeaks(peak), settings, monitor);
-	}
-
-	@Override
-	public IProcessingInfo<T> identify(IPeakMSD peak, IProgressMonitor monitor) {
-
-		return identify(peak, getSettings(PreferenceSupplier.P_PEAK_IDENTIFIER_LIST_MSD), monitor);
-	}
-
-	@Override
-	public IProcessingInfo<T> identify(List<? extends IPeakMSD> peaks, IProgressMonitor monitor) {
-
-		return identify(peaks, getSettings(PreferenceSupplier.P_PEAK_IDENTIFIER_LIST_MSD), monitor);
-	}
-
-	@Override
-	public IProcessingInfo<T> identify(IChromatogramSelectionMSD chromatogramSelectionMSD, IProgressMonitor monitor) {
-
-		return identify(extractPeaks(chromatogramSelectionMSD), monitor);
-	}
-
-	@Override
-	public IProcessingInfo<T> identify(IChromatogramSelectionMSD chromatogramSelectionMSD, IPeakIdentifierSettingsMSD peakIdentifierSettings, IProgressMonitor monitor) {
-
-		return identify(extractPeaks(chromatogramSelectionMSD), peakIdentifierSettings, monitor);
 	}
 }
