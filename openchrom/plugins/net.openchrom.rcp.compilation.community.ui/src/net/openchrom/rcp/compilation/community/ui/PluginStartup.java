8a3fdf7470d44a3dad3805d274d102feb4b36d7d
Dr. Philip Wenig <philip.wenig@openchrom.net>
2019-10-28 06:35:34 +0100

    The update sites are not needed anymore at this section.

--- openchrom/plugins/net.openchrom.rcp.compilation.community.ui/src/net/openchrom/rcp/compilation/community/ui/PluginStartup.java
+++ openchrom/plugins/net.openchrom.rcp.compilation.community.ui/src/net/openchrom/rcp/compilation/community/ui/PluginStartup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2018 Lablicate GmbH.
+ * Copyright (c) 2014, 2019 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -11,10 +11,6 @@
  *******************************************************************************/
 package net.openchrom.rcp.compilation.community.ui;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.eclipse.chemclipse.support.ui.p2.UpdateSiteSupport;
 import org.eclipse.ui.IStartup;
 
 public class PluginStartup implements IStartup {
@@ -22,7 +18,7 @@ public class PluginStartup implements IStartup {
 	@Override
 	public void earlyStartup() {
 
-		UpdateSiteSupport updateSiteSupport = new UpdateSiteSupport();
+		// UpdateSiteSupport updateSiteSupport = new UpdateSiteSupport();
 		/*
 		 * Set unique new update sites.
 		 * Marketplace Plugins
@@ -30,12 +26,12 @@ public class PluginStartup implements IStartup {
 		 * additionally installed plugins can be updated.
 		 * updateSiteSupport.removeProvisioningRepositories();
 		 */
-		Map<String, String> updateSites = new HashMap<String, String>();
-		updateSites.put("OpenChrom Community Edition", "http://update.openchrom.net/repositories/community/1.4.x/repository");
-		updateSites.put("OpenChrom 3rd Party Libraries", "http://update.openchrom.net/repositories/community/1.4.x/plugins/openchrom3rdpl"); // 3rd Party Libraries
-		updateSites.put("OpenChrom Keys", "http://update.openchrom.net/repositories/community/1.4.x/plugins/openchromkeys"); // Keys Support
-		updateSites.put("OpenChrom Icons", "http://update.openchrom.net/repositories/community/1.4.x/plugins/enterprisesupport"); // Enterprise Icons
-		updateSites.put("OpenChrom xIdent", "http://update.openchrom.net/repositories/community/1.4.x/plugins/xident"); // xIdent Support
-		updateSiteSupport.addProvisioningRepositories(updateSites);
+		// Map<String, String> updateSites = new HashMap<String, String>();
+		// updateSites.put("OpenChrom Community Edition", "http://update.openchrom.net/repositories/community/1.4.x/repository");
+		// updateSites.put("OpenChrom 3rd Party Libraries", "http://update.openchrom.net/repositories/community/1.4.x/plugins/openchrom3rdpl"); // 3rd Party Libraries
+		// updateSites.put("OpenChrom Keys", "http://update.openchrom.net/repositories/community/1.4.x/plugins/openchromkeys"); // Keys Support
+		// updateSites.put("OpenChrom Icons", "http://update.openchrom.net/repositories/community/1.4.x/plugins/enterprisesupport"); // Enterprise Icons
+		// updateSites.put("OpenChrom xIdent", "http://update.openchrom.net/repositories/community/1.4.x/plugins/xident"); // xIdent Support
+		// updateSiteSupport.addProvisioningRepositories(updateSites);
 	}
 }
