e26840f9e4cf33d2a01954e3c168ce94856a9a99
Alexander Stark <alexander.stark@lablicate.com>
2019-11-05 17:58:11 +0100

    FOODAUTH-164 Add more code for construction of ridges

--- openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/peakdetection/WaveletPeakDetectorRidges.java
+++ openchrom/plugins/net.openchrom.nmr.processing.supplier.base/src/net/openchrom/nmr/processing/peakdetection/WaveletPeakDetectorRidges.java
@@ -12,11 +12,16 @@
 package net.openchrom.nmr.processing.peakdetection;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.ejml.simple.SimpleMatrix;
+
+import net.openchrom.nmr.processing.supplier.base.core.UtilityFunctions;
 
 public class WaveletPeakDetectorRidges {
 
@@ -26,25 +31,20 @@ public class WaveletPeakDetectorRidges {
 		int numberOfColumns = localMaxima.numCols();
 		int numberOfRows = localMaxima.numRows();
 
-		List<Integer> maxIndexCurrent = WaveletPeakDetectorRidgesUtils.getMaxCurrentIndex(localMaxima, numberOfColumns);
-
-		int[] columnIndex = WaveletPeakDetectorRidgesUtils.generateColumnIndex(numberOfColumns);
-
+		List<Integer> currentMaxIndex = WaveletPeakDetectorRidgesUtils.getMaxCurrentIndex(localMaxima, numberOfColumns);
 		LinkedHashMap<Integer, List<Integer>> ridgeList = new LinkedHashMap<Integer, List<Integer>>();
 		LinkedHashMap<Integer, Integer> peakStatus = new LinkedHashMap<Integer, Integer>();
-		for(Integer key : maxIndexCurrent) {
+		for(Integer key : currentMaxIndex) {
 			List<Integer> values = new ArrayList<Integer>();
 			values.add(key);
 			ridgeList.put(key, values);
 			peakStatus.put(key, 0);
 		}
-
-		List<Integer> orphanRidgeList = new ArrayList<Integer>();
-		List<Integer> orphanRidgeName = new ArrayList<Integer>();
-
-		int numberOfLevels = columnIndex.length;
+		LinkedHashMap<String, List<Integer>> orphanRidgeList = new LinkedHashMap<String, List<Integer>>();
 
 		// main loop
+		int[] columnIndex = WaveletPeakDetectorRidgesUtils.generateColumnIndex(numberOfColumns);
+		int numberOfLevels = columnIndex.length;
 		for(int i = 0; i < numberOfLevels; i++) {
 			int localColumn = columnIndex[i];
 			int localScale = psiScales[localColumn];
@@ -54,20 +54,24 @@ public class WaveletPeakDetectorRidges {
 				}
 				continue;
 			}
-			if(maxIndexCurrent.size() == 0) {
-				maxIndexCurrent = WaveletPeakDetectorRidgesUtils.getMaxCurrentIndex(localMaxima, localColumn);
+			if(currentMaxIndex.size() == 0) {
+				currentMaxIndex = WaveletPeakDetectorRidgesUtils.getMaxCurrentIndex(localMaxima, localColumn);
 				continue;
 			}
 			// slide window size
 			int localWindowSize = WaveletPeakDetectorMaximaUtils.calculateWindowSize(localScale, configuration);
 
 			// prüfen ob richtiger Typ
-			LinkedHashMap<Integer, Integer> selectedPeaks = new LinkedHashMap<Integer, Integer>();
-			LinkedHashMap<Integer, Integer> removePeak = new LinkedHashMap<Integer, Integer>();
+			// LinkedHashMap<Integer, Integer> selectedPeaks = new LinkedHashMap<Integer,
+			// Integer>();
+			// LinkedHashMap<Integer, Integer> removePeak = new LinkedHashMap<Integer,
+			// Integer>();
+			List<Integer> localSelectedPeaks = new ArrayList<Integer>();
+			List<Integer> localRemovePeaks = new ArrayList<Integer>();
 
 			// inner loop
-			for(int k = 0; k < maxIndexCurrent.size(); k++) {
-				int localIndex = maxIndexCurrent.get(k);
+			for(int k = 0; k < currentMaxIndex.size(); k++) {
+				int localIndex = currentMaxIndex.get(k);
 				int start = ((localIndex - localWindowSize < 1) ? 1 : localIndex - localWindowSize);
 				int end = ((localIndex + localWindowSize > numberOfRows) ? numberOfRows : localIndex + localWindowSize);
 				//
@@ -80,10 +84,64 @@ public class WaveletPeakDetectorRidges {
 				}
 
 				if(currentIndex.size() == 0) {
+					int localStatus = peakStatus.get(localIndex);
+					if(localStatus == 0) {
+						localStatus = gapThreshold + 1;
+					}
+					if(localStatus > gapThreshold & localScale >= 2) {
+						List<Integer> tempList = ridgeList.get(localIndex);
+						List<Integer> values = tempList.subList(0, tempList.size() - localStatus);
+						String key = String.valueOf(localColumn + localStatus + 1) + "_" + String.valueOf(localIndex);
+						orphanRidgeList.put(key, values);
+						localRemovePeaks.add(localIndex);
+						continue;
+					} else {
+						currentIndex.add(localIndex);
+						peakStatus.put(localIndex, localStatus + 1);
+					}
+				} else {
+					peakStatus.put(localIndex, 0);
+					if(currentIndex.size() >= 2) {
+						double[] tempIndex = new double[currentIndex.size()];
+						for(int j = 0; j < currentIndex.size(); j++) {
+							tempIndex[j] = Math.abs(currentIndex.get(j) - localIndex);
+						}
 
+						double minValue = UtilityFunctions.getMinValueOfArray(tempIndex);
+						for(Integer element : currentIndex) {
+							if(!element.equals(Double.valueOf(minValue).intValue())) {
+								currentIndex.remove(element);
+							}
+						}
+					}
 				}
+				ridgeList.put(localIndex, currentIndex);
+				localSelectedPeaks.add(currentIndex.get(0));
+			} // end inner loop
+
+			// Remove disconnected lines
+			if(localRemovePeaks.size() > 0) {
+				for(Integer removePeak : localRemovePeaks) {
+					if(ridgeList.containsKey(removePeak)) {
+						ridgeList.remove(removePeak);
+						peakStatus.remove(removePeak);
+					}
+				}
+			}
+
+			// Check for duplicated selected peaks;only keep the one with the longest path
+			List<Integer> localUniquePeaks = localSelectedPeaks.stream().distinct().collect(Collectors.toList());
+			if(localSelectedPeaks.size() > localUniquePeaks.size()) {
+				//
+				List<Integer> localDiplicatedPeaks = localSelectedPeaks.stream().filter(not(new HashSet<>(localUniquePeaks)::contains)).collect(Collectors.toList());
+
 			}
 		}
 		return null;
+	}
+
+	private static <T> Predicate<T> not(Predicate<T> predicate) {
+
+		return predicate.negate();
 	}
 }
