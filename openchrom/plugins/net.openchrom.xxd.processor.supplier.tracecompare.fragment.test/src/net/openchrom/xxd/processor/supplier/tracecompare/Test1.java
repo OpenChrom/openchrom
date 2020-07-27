d9140ea122edf97bde9685921e6e905c7a7d6e1d
Dr. Philip Wenig <philip.wenig@openchrom.net>
2020-07-27 12:31:03 +0200

    A default test case has been added.

---
+++ openchrom/plugins/net.openchrom.xxd.processor.supplier.tracecompare.fragment.test/src/net/openchrom/xxd/processor/supplier/tracecompare/Test1.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.xxd.processor.supplier.tracecompare;
+
+import junit.framework.TestCase;
+
+public class Test1 extends TestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+
+		super.setUp();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+
+		super.tearDown();
+	}
+
+	public void test1() {
+
+		assertTrue(true); // The build complains about a missing test case here.
+	}
+}
