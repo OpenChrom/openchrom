5ad459d006cec86a5b314151ebaff09d65c91a9a
Matthias Mailänder <matthias@mailaender.name>
2020-09-26 18:58:36 +0200

    Add missing copyright headers from before refactoring.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.mgf/src/net/openchrom/msd/converter/supplier/mgf/converter/model/IScanMSDFactory.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mgf/src/net/openchrom/msd/converter/supplier/mgf/converter/model/IScanMSDFactory.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Lablicate UG (haftungsbeschränkt).
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Dr. Alexander Kerner - initial API and implementation
+ *******************************************************************************/
+
 package net.openchrom.msd.converter.supplier.mgf.converter.model;
 
 import org.eclipse.chemclipse.msd.model.core.IScanMSD;
