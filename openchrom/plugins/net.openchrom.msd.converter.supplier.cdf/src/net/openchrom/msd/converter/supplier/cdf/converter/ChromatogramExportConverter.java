cc178e0d0f0398b2d2a1d32509bc53a4ae9f5a22
Philip Wenig <philip.wenig@openchrom.net>
2020-11-24 16:52:48 +0100

    Warnings regarding generic annotations have been addressed.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.cdf/src/net/openchrom/msd/converter/supplier/cdf/converter/ChromatogramExportConverter.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.cdf/src/net/openchrom/msd/converter/supplier/cdf/converter/ChromatogramExportConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2019 Lablicate GmbH.
+ * Copyright (c) 2013, 2020 Lablicate GmbH.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
@@ -26,17 +26,16 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import net.openchrom.msd.converter.supplier.cdf.internal.converter.SpecificationValidator;
 import net.openchrom.msd.converter.supplier.cdf.io.ChromatogramWriter;
 
-@SuppressWarnings({"unchecked", "rawtypes"})
 public class ChromatogramExportConverter extends AbstractChromatogramExportConverter implements IChromatogramExportConverter {
 
 	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
 	private static final String DESCRIPTION = "NetCDF Export Converter";
 
 	@Override
-	public IProcessingInfo convert(File file, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
+	public IProcessingInfo<File> convert(File file, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
 
 		file = SpecificationValidator.validateSpecification(file);
-		IProcessingInfo processingInfo = super.validate(file);
+		IProcessingInfo<File> processingInfo = super.validate(file);
 		if(!processingInfo.hasErrorMessages() && chromatogram instanceof IChromatogramMSD) {
 			IChromatogramMSD chromatogramMSD = (IChromatogramMSD)chromatogram;
 			IChromatogramMSDWriter writer = new ChromatogramWriter();
--- openchrom/plugins/net.openchrom.msd.converter.supplier.mgf/src/net/openchrom/msd/converter/supplier/mgf/converter/ChromatogramExportConverter.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mgf/src/net/openchrom/msd/converter/supplier/mgf/converter/ChromatogramExportConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2018 Lablicate GmbH.
+ * Copyright (c) 2016, 2020 Lablicate GmbH.
  * 
  * All rights reserved.
  * This program and the accompanying materials are made available under the
@@ -32,10 +32,10 @@ public class ChromatogramExportConverter extends AbstractChromatogramExportConve
 	private static final String DESCRIPTION = "MGF Export Converter";
 
 	@Override
-	public IProcessingInfo convert(File file, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
+	public IProcessingInfo<File> convert(File file, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
 
 		file = SpecificationValidator.validateSpecification(file);
-		IProcessingInfo processingInfo = super.validate(file);
+		IProcessingInfo<File> processingInfo = super.validate(file);
 		if(!processingInfo.hasErrorMessages() && chromatogram instanceof IChromatogramMSD) {
 			IChromatogramMSD chromatogramMSD = (IChromatogramMSD)chromatogram;
 			ChromatogramWriter writer = new ChromatogramWriter();
