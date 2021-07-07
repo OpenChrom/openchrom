9f2a59dff26962a92ba3b834140afc7ca47ac0c0
Philip Wenig <philip.wenig@openchrom.net>
2021-07-07 09:41:27 +0200

    Fixed header.

--- openchrom/plugins/net.openchrom.csd.converter.supplier.cdf/src/net/openchrom/csd/converter/supplier/cdf/converter/ChromatogramImportConverterTSD.java
+++ openchrom/plugins/net.openchrom.csd.converter.supplier.cdf/src/net/openchrom/csd/converter/supplier/cdf/converter/ChromatogramImportConverterTSD.java
@@ -1,17 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2021 Lablicate GmbH.
  *
- * All rights reserved. This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  * Philip Wenig - initial API and implementation
