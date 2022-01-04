feb4b0c679db66e10d8d4e14075fcdc24220fc94
Philip Wenig <philip.wenig@openchrom.net>
2022-01-04 15:12:22 +0100

    The copyright header was wrong respectively misleading, hence adjusted.
    The editor has been copied from PeakDetectorFieldEditor.java in the
    commit 621ded44cbc9f76109f29f8b48150ea6503308db.

--- openchrom/plugins/net.openchrom.xxd.process.supplier.templates.ui/src/net/openchrom/xxd/process/supplier/templates/ui/swt/TemplatePeakListEditor.java
+++ openchrom/plugins/net.openchrom.xxd.process.supplier.templates.ui/src/net/openchrom/xxd/process/supplier/templates/ui/swt/TemplatePeakListEditor.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2020 Lablicate GmbH.
- *
+ * Copyright (c) 2018, 2022 Lablicate GmbH.
+ * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * Christoph Läubrich - initial API and implementation
+ * Dr. Philip Wenig - initial API and implementation
+ * Christoph Läubrich - refactor to use editor
  *******************************************************************************/
 package net.openchrom.xxd.process.supplier.templates.ui.swt;
 
