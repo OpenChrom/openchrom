77f248950e0fef79c294d41e508307fadc72f9de
Matthias Mailänder <matthias.mailaender@lablicate.com>
2022-03-29 12:12:57 +0200

    Code cleanup

--- openchrom/plugins/net.openchrom.csd.converter.supplier.cdf/src/net/openchrom/csd/converter/supplier/cdf/io/support/AbstractCDFChromatogramArrayReader.java
+++ openchrom/plugins/net.openchrom.csd.converter.supplier.cdf/src/net/openchrom/csd/converter/supplier/cdf/io/support/AbstractCDFChromatogramArrayReader.java
@@ -93,7 +93,7 @@ public abstract class AbstractCDFChromatogramArrayReader implements IAbstractCDF
 			retentionTimeScaleFactor = 1;
 		}
 		/*
-		 * Not all supplier store a run time lenght.
+		 * Not all supplier store a run time length.
 		 */
 		variable = CDFConstants.VARIABLE_ACTUAL_RUN_TIME_LENGTH;
 		valueRunTimeLength = chromatogram.findVariable(variable);
