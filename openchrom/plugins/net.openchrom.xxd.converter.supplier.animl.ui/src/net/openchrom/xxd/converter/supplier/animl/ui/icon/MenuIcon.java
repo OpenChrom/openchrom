31bb30afdfebff5a502936b2963fba722fda7772
Matthias Mailänder <matthias.mailaender@lablicate.com>
2022-08-02 14:53:40 +0200

    Add XML and PDF icons to the right-click menu

---
+++ openchrom/plugins/net.openchrom.xxd.converter.supplier.animl.ui/src/net/openchrom/xxd/converter/supplier/animl/ui/icon/MenuIcon.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Matthias Mailänder - support process method resume option
+ *******************************************************************************/
+package net.openchrom.xxd.converter.supplier.animl.ui.icon;
+
+import org.eclipse.chemclipse.rcp.ui.icons.core.ApplicationImageFactory;
+import org.eclipse.chemclipse.rcp.ui.icons.core.IApplicationImage;
+import org.eclipse.chemclipse.rcp.ui.icons.core.IApplicationImageProvider;
+import org.eclipse.chemclipse.xxd.process.ui.menu.IMenuIcon;
+import org.eclipse.swt.graphics.Image;
+
+public class MenuIcon implements IMenuIcon {
+
+	@Override
+	public Image getImage() {
+
+		return ApplicationImageFactory.getInstance().getImage(IApplicationImage.IMAGE_XML_FILE, IApplicationImageProvider.SIZE_16x16);
+	}
+}
