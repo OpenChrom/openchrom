31a5c64ea2d4ff3c1eb560f62180dde73eecab99
Matthias Mailänder <matthias.mailaender@lablicate.com>
2022-08-18 12:21:58 +0200

    Add a template process icon.

--- openchrom/plugins/net.openchrom.xxd.process.supplier.templates.ui/src/net/openchrom/xxd/process/supplier/templates/ui/Activator.java
+++ openchrom/plugins/net.openchrom.xxd.process.supplier.templates.ui/src/net/openchrom/xxd/process/supplier/templates/ui/Activator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2019 Lablicate GmbH.
+ * Copyright (c) 2018, 2022 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,13 +8,18 @@
  * 
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
+ * Matthias Mailänder - icons
  *******************************************************************************/
 package net.openchrom.xxd.process.supplier.templates.ui;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.chemclipse.support.ui.activator.AbstractActivatorUI;
 import org.osgi.framework.BundleContext;
 
 import net.openchrom.xxd.process.supplier.templates.preferences.PreferenceSupplier;
+import net.openchrom.xxd.process.supplier.templates.ui.icon.Icon;
 
 public class Activator extends AbstractActivatorUI {
 
@@ -25,6 +30,7 @@ public class Activator extends AbstractActivatorUI {
 		super.start(context);
 		plugin = this;
 		initializePreferenceStore(PreferenceSupplier.INSTANCE());
+		initializeImageRegistry(getImageHashMap());
 	}
 
 	public void stop(BundleContext context) throws Exception {
@@ -36,5 +42,12 @@ public class Activator extends AbstractActivatorUI {
 	public static AbstractActivatorUI getDefault() {
 
 		return plugin;
+	}
+
+	private Map<String, String> getImageHashMap() {
+
+		Map<String, String> imageHashMap = new HashMap<>();
+		imageHashMap.put(Icon.TEMPLATE, "icons/16x16/template.gif"); // $NON-NLS-1$
+		return imageHashMap;
 	}
 }
