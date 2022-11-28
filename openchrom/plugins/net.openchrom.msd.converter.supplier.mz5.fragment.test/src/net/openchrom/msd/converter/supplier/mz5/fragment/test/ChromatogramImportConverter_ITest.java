a162652dd1f9ec95359928f6e1f4184b39743a2a
Matthias Mailänder <matthias.mailaender@lablicate.com>
2022-11-28 21:40:29 +0100

    Add a small .mz5 test case.

---
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mz5.fragment.test/src/net/openchrom/msd/converter/supplier/mz5/fragment/test/ChromatogramImportConverter_ITest.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.msd.converter.supplier.mz5.fragment.test;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.core.runtime.NullProgressMonitor;
+
+import junit.framework.TestCase;
+import net.openchrom.msd.converter.supplier.mz5.PathResolver;
+import net.openchrom.msd.converter.supplier.mz5.converter.ChromatogramImportConverter;
+
+public class ChromatogramImportConverter_ITest extends TestCase {
+
+	private IChromatogramMSD chromatogram;
+
+	@Override
+	protected void setUp() throws Exception {
+
+		super.setUp();
+		File file = new File(PathResolver.getAbsolutePath(TestPathHelper.TESTFILE));
+		ChromatogramImportConverter importConverter = new ChromatogramImportConverter();
+		IProcessingInfo<IChromatogramMSD> processingInfo = importConverter.convert(file, new NullProgressMonitor());
+		chromatogram = processingInfo.getProcessingResult();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+
+		chromatogram = null;
+		super.tearDown();
+	}
+
+	public void testLoading() {
+
+		assertNotNull(chromatogram);
+	}
+
+	public void testScans() {
+
+		assertEquals(chromatogram.getNumberOfScans(), 26);
+		assertEquals(chromatogram.getScan(10).getTotalSignal(), 105525.87f);
+		assertEquals(chromatogram.getScan(26).getRetentionTime(), 16639);
+	}
+}
