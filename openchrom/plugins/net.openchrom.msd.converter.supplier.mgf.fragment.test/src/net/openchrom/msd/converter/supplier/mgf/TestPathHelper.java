3daa7e852be79f510f8367f62b4024a7e8c86afd
Matthias Mailänder <matthias.mailaender@lablicate.com>
2022-11-30 13:30:03 +0100

    Remove unused and wrong converter IDs.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.mgf.fragment.test/src/net/openchrom/msd/converter/supplier/mgf/TestPathHelper.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mgf.fragment.test/src/net/openchrom/msd/converter/supplier/mgf/TestPathHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2018 Lablicate GmbH.
+ * Copyright (c) 2015, 2022 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -18,7 +18,6 @@ package net.openchrom.msd.converter.supplier.mgf;
  */
 public class TestPathHelper extends PathResolver {
 
-	public static final String CONVERTER_ID = "org.eclipse.chemclipse.msd.converter.supplier.mgf";
 	/*
 	 * EXPORT
 	 */
