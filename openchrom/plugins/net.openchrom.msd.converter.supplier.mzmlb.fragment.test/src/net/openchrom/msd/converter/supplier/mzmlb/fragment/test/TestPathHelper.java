96d6ad09d62b3c5fdb81bbb95faad6f54b1fd7f7
Matthias Mailänder <matthias.mailaender@lablicate.com>
2022-11-30 13:34:11 +0100

    Add a small unit test for .mzMLb

---
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mzmlb.fragment.test/src/net/openchrom/msd/converter/supplier/mzmlb/fragment/test/TestPathHelper.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.msd.converter.supplier.mzmlb.fragment.test;
+
+import net.openchrom.msd.converter.supplier.mzmlb.PathResolver;
+
+/**
+ * THIS CLASS IS NOT SUITED FOR PRODUCTIVE USE!<br/>
+ * IT IS A TESTCLASS!
+ */
+public class TestPathHelper extends PathResolver {
+
+	/*
+	 * IMPORT
+	 */
+	public static final String TESTFILE = "testData/files/import/test.mzMLb";
+}
