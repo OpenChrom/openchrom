bca872ca19495638f73ec8a3a63dcc5c0e887839
Philip Wenig <philip.wenig@openchrom.net>
2023-07-01 10:23:03 +0200

    Amend #391 - Added support for the MicrobeNet MALDI XML format
    Both import converter implementation as well as magic number matcher are
    mandatory, even though only export is activated.

---
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.microbenet/src/net/openchrom/msd/converter/supplier/microbenet/converter/DatabaseImportConverter.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.msd.converter.supplier.microbenet.converter;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.msd.converter.database.AbstractDatabaseImportConverter;
+import org.eclipse.chemclipse.msd.model.core.IMassSpectra;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.chemclipse.processing.core.ProcessingInfo;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class DatabaseImportConverter extends AbstractDatabaseImportConverter {
+
+	@Override
+	public IProcessingInfo<IMassSpectra> convert(File file, IProgressMonitor monitor) {
+
+		IProcessingInfo<IMassSpectra> processingInfo = new ProcessingInfo<>();
+		processingInfo.addWarnMessage("MicrobeNet", "This converter doesn't support the import of *.xml files.");
+		return processingInfo;
+	}
+}
