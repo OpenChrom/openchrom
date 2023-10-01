1d61e07fb1b997eeb65b77d782731c1aa3fc754d
Egon Willighagen <egon.willighagen@gmail.com>
2023-10-01 07:36:23 +0200

    Added the missing 2023 copyright line

--- openchrom/plugins/net.openchrom.xxd.identifier.supplier.cdk/src/net/openchrom/xxd/identifier/supplier/cdk/converter/CDKSmilesToMoleculeConverter.java
+++ openchrom/plugins/net.openchrom.xxd.identifier.supplier.cdk/src/net/openchrom/xxd/identifier/supplier/cdk/converter/CDKSmilesToMoleculeConverter.java
@@ -1,6 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2013, 2022 Marwin Wollschläger, Lablicate GmbH.
- * 
+ * Copyright (c) 2023 Egon Willighagen <egonw@users.sf.net>
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
