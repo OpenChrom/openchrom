e72ddeafd6e00e3091fc7f4b173b32898993c551
Matthias Mailänder <matthias.mailaender@lablicate.com>
2023-12-19 21:33:51 +0100

    Add a test case.

---
+++ openchrom/tests/net.openchrom.msd.converter.supplier.mzdb.fragment.test/src/net/openchrom/msd/converter/supplier/mzdb/fragment/test/ChromatogramImportConverter_ITest.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2022, 2023 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.msd.converter.supplier.mzdb.fragment.test;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.chemclipse.support.history.IEditInformation;
+import org.eclipse.core.runtime.NullProgressMonitor;
+
+import net.openchrom.msd.converter.supplier.mzdb.PathResolver;
+import net.openchrom.msd.converter.supplier.mzdb.converter.ChromatogramImportConverter;
+
+import junit.framework.TestCase;
+
+public class ChromatogramImportConverter_ITest extends TestCase {
+
+	private IChromatogramMSD chromatogram;
+
+	@Override
+	protected void setUp() throws Exception {
+
+		super.setUp();
+		File file = new File(PathResolver.getAbsolutePath(TestPathHelper.TESTFILE));
+		ChromatogramImportConverter importConverter = new ChromatogramImportConverter();
+		IProcessingInfo<IChromatogramMSD> processingInfo = importConverter.convert(file, new NullProgressMonitor());
+		chromatogram = processingInfo.getProcessingResult();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+
+		chromatogram = null;
+		super.tearDown();
+	}
+
+	public void testLoading() {
+
+		assertNotNull(chromatogram);
+		assertEquals("1", chromatogram.getSampleName());
+	}
+
+	public void testScans() {
+
+		assertEquals(48, chromatogram.getNumberOfScans());
+		assertEquals(1.6795854E7f, chromatogram.getScan(1).getTotalSignal());
+		assertEquals(474, chromatogram.getScan(2).getRetentionTime());
+	}
+
+	public void testHistory() {
+
+		assertEquals(2, chromatogram.getEditHistory().size());
+		IEditInformation info = chromatogram.getEditHistory().get(1);
+		assertEquals("mzML to mzDB conversion", info.getDescription());
+		assertEquals("Thermo2mzDB", info.getEditor());
+	}
+}
---
+++ openchrom/tests/net.openchrom.msd.converter.supplier.mzdb.fragment.test/src/net/openchrom/msd/converter/supplier/mzdb/fragment/test/TestPathHelper.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.msd.converter.supplier.mzdb.fragment.test;
+
+import net.openchrom.msd.converter.supplier.mzdb.PathResolver;
+
+/**
+ * THIS CLASS IS NOT SUITED FOR PRODUCTIVE USE!<br/>
+ * IT IS A TESTCLASS!
+ */
+public class TestPathHelper extends PathResolver {
+
+	/*
+	 * IMPORT
+	 */
+	public static final String TESTFILE = "testData/files/import/small.mzDB";
+}
