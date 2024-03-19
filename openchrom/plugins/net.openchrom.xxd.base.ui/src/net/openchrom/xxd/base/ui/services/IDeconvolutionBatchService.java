78fc70fabfbc1b7a1091643a19cd8dd1d3405ce5
Lorenz Gerber <lorenz.gerber@lablicate.com>
2024-03-19 14:42:48 +0800

    adjust return type of service
    Signed-off-by: Lorenz Gerber <lorenz.gerber@lablicate.com>

--- openchrom/plugins/net.openchrom.xxd.base.ui/src/net/openchrom/xxd/base/ui/services/IDeconvolutionBatchService.java
+++ openchrom/plugins/net.openchrom.xxd.base.ui/src/net/openchrom/xxd/base/ui/services/IDeconvolutionBatchService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023 Lablicate GmbH.
+ * Copyright (c) 2023, 2024 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -14,9 +14,9 @@ package net.openchrom.xxd.base.ui.services;
 import java.util.List;
 
 import org.eclipse.chemclipse.model.core.IChromatogram;
-import org.eclipse.chemclipse.model.core.IPeak;
 import org.eclipse.chemclipse.model.ranges.TimeRanges;
 import org.eclipse.chemclipse.model.types.DataType;
+import org.eclipse.chemclipse.msd.model.core.IPeakMSD;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.ui.IWorkbenchPreferencePage;
@@ -31,7 +31,7 @@ public interface IDeconvolutionBatchService {
 
 	DataType getDataType();
 
-	IProcessingInfo<List<List<? extends IPeak>>> calculate(List<IChromatogram<?>> chromatograms, TimeRanges timeRanges, IProgressMonitor monitor);
+	IProcessingInfo<List<List<List<IPeakMSD>>>> calculate(List<IChromatogram<?>> chromatograms, TimeRanges timeRanges, IProgressMonitor monitor);
 
 	Class<? extends IWorkbenchPreferencePage> getPreferencePage();
 }
