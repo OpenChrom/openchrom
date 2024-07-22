b996594c7b0645af5b3bc4226b01df4cfa8ae633
Philip Wenig <philip.wenig@openchrom.net>
2024-07-22 09:04:57 +0200

    Fixed #522 - Specific Editors - set a width hint

--- openchrom/plugins/net.openchrom.chromatogram.xxd.report.supplier.csv.ui/src/net/openchrom/chromatogram/xxd/report/supplier/csv/ui/services/ReportColumnsAnnotationService.java
+++ openchrom/plugins/net.openchrom.chromatogram.xxd.report.supplier.csv.ui/src/net/openchrom/chromatogram/xxd/report/supplier/csv/ui/services/ReportColumnsAnnotationService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2023 Lablicate GmbH.
+ * Copyright (c) 2021, 2024 Lablicate GmbH.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -34,6 +34,7 @@ public class ReportColumnsAnnotationService extends ReportColumnsSerializationSe
 		reportColumnEditor.setToolTipText(description);
 		GridData gridData = new GridData(GridData.FILL_BOTH);
 		gridData.heightHint = 200;
+		gridData.widthHint = 500;
 		reportColumnEditor.setLayoutData(gridData);
 		//
 		if(currentSelection instanceof String text) {
