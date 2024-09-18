855fa78e69eb13dc140da982f916ce9fe41e13e7
Matthias Mailänder <matthias.mailaender@lablicate.com>
2024-09-18 18:15:32 +0200

    Avoid deprecated API.

--- openchrom/plugins/net.openchrom.csd.converter.supplier.cdf/src/net/openchrom/csd/converter/supplier/cdf/io/ChromatogramWriterCSD.java
+++ openchrom/plugins/net.openchrom.csd.converter.supplier.cdf/src/net/openchrom/csd/converter/supplier/cdf/io/ChromatogramWriterCSD.java
@@ -13,7 +13,6 @@ package net.openchrom.csd.converter.supplier.cdf.io;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import org.eclipse.chemclipse.converter.exceptions.FileIsNotWriteableException;
 import org.eclipse.chemclipse.converter.io.AbstractChromatogramWriter;
@@ -24,15 +23,13 @@ import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 import net.openchrom.csd.converter.supplier.cdf.io.support.AttributeSupport;
-import net.openchrom.csd.converter.supplier.cdf.io.support.CDFConstants;
 import net.openchrom.csd.converter.supplier.cdf.io.support.DimensionSupport;
 import net.openchrom.csd.converter.supplier.cdf.io.support.IDataEntry;
 
 import ucar.ma2.InvalidRangeException;
-import ucar.nc2.Dimension;
-import ucar.nc2.NetcdfFileWriter;
+import ucar.nc2.write.NetcdfFormatWriter;
+import ucar.nc2.write.NetcdfFormatWriter.Builder;
 
-@SuppressWarnings("deprecation")
 public class ChromatogramWriterCSD extends AbstractChromatogramWriter implements IChromatogramCSDWriter {
 
 	private static final Logger logger = Logger.getLogger(ChromatogramWriterCSD.class);
@@ -46,34 +43,21 @@ public class ChromatogramWriterCSD extends AbstractChromatogramWriter implements
 
 	private void writeCDFChromatogram(File file, IChromatogramCSD chromatogram, IProgressMonitor monitor) throws IOException {
 
-		NetcdfFileWriter cdfChromatogram = NetcdfFileWriter.createNew(file.getAbsolutePath(), false);
-		DimensionSupport dimensionSupport = new DimensionSupport(cdfChromatogram, chromatogram);
-		AttributeSupport.setAttributes(cdfChromatogram, chromatogram);
-		Dimension numberOfScans = dimensionSupport.getNumberOfScans();
+		Builder builder = NetcdfFormatWriter.createNewNetcdf3(file.getAbsolutePath());
+		DimensionSupport dimensionSupport = new DimensionSupport(builder, chromatogram);
+		AttributeSupport.setAttributes(builder, chromatogram);
 		//
-		dimensionSupport.addVariableDoubleD1(CDFConstants.VARIABLE_ORDINATE_VALUES, numberOfScans, dimensionSupport.NULL_VALUE_DOUBLE);
 		dimensionSupport.addVariableOrdinateValues();
-		try {
-			cdfChromatogram.create();
-			ArrayList<IDataEntry> dataEntries = dimensionSupport.getDataEntries();
-			for(IDataEntry entry : dataEntries) {
+		//
+		try (NetcdfFormatWriter writer = builder.build()) {
+			for(IDataEntry entry : dimensionSupport.getDataEntries()) {
 				monitor.subTask(entry.getVarName());
-				cdfChromatogram.write(entry.getVarName(), entry.getValues());
+				writer.write(entry.getVarName(), entry.getValues());
 			}
-			cdfChromatogram.close();
-			cdfChromatogram = null;
 		} catch(IOException e) {
 			logger.warn(e);
 		} catch(InvalidRangeException e) {
 			logger.warn(e);
-		} finally {
-			if(cdfChromatogram != null) {
-				try {
-					cdfChromatogram.close();
-				} catch(IOException e) {
-					logger.warn(e);
-				}
-			}
 		}
 	}
 }
