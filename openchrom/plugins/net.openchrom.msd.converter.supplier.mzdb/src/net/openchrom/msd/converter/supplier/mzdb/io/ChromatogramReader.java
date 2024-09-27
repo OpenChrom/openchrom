a97062ce6bde8b78f99c3fc4136219b5bc997fe7
Matthias Mailänder <matthias@mailaender.name>
2024-09-27 20:34:53 +0200

    Modularize and use auto-closeables.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.mzdb/src/net/openchrom/msd/converter/supplier/mzdb/io/ChromatogramReader.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mzdb/src/net/openchrom/msd/converter/supplier/mzdb/io/ChromatogramReader.java
@@ -64,60 +64,87 @@ public class ChromatogramReader extends AbstractChromatogramReader implements IC
 		}
 		try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + file.toString())) {
 			try (Statement statement = connection.createStatement()) {
-				//
-				ResultSet mzdbResultSet = statement.executeQuery("SELECT * FROM mzdb;");
-				long epoch = mzdbResultSet.getLong("creation_timestamp") * 1000; // to milliseconds
 				chromatogram = new VendorChromatogram();
 				chromatogram.setFile(file);
-				chromatogram.setDate(new Date(epoch));
-				float version = mzdbResultSet.getFloat("version");
-				if(version > 0.7) {
-					logger.warn("The .mzDB file may be incompatible.");
-				}
-				mzdbResultSet.close();
-				//
-				ResultSet nameResultSet = statement.executeQuery("SELECT name FROM sample;");
-				String sampleName = nameResultSet.getString(1);
-				chromatogram.setSampleName(sampleName);
-				nameResultSet.close();
-				//
-				ResultSet dataProcessingSet = statement.executeQuery("SELECT name FROM data_processing ORDER BY id;");
-				List<String> dataProcessing = new ArrayList<>();
-				while(dataProcessingSet.next()) {
-					dataProcessing.add(dataProcessingSet.getString("name"));
-				}
-				dataProcessingSet.close();
-				//
-				ResultSet softwareResultSet = statement.executeQuery("SELECT name FROM software ORDER BY id;");
-				List<String> software = new ArrayList<>();
-				while(softwareResultSet.next()) {
-					software.add(softwareResultSet.getString("name"));
-				}
-				softwareResultSet.close();
-				//
-				int size = Math.min(dataProcessing.size(), software.size());
-				for(int i = 0; i < size; i++) {
-					chromatogram.getEditHistory().add(new EditInformation(dataProcessing.get(i), software.get(i)));
-				}
-				//
-				ResultSet spectrumResultSet = statement.executeQuery("SELECT * FROM spectrum;");
-				while(spectrumResultSet.next()) {
-					IVendorScan scan = new VendorScan();
-					scan.setScanNumber(spectrumResultSet.getInt("cycle"));
-					scan.setIdentifier(spectrumResultSet.getString("title"));
-					scan.setRetentionTime(Math.round(spectrumResultSet.getFloat("time") * 1000)); // s to ms
-					scan.setMassSpectrometer(spectrumResultSet.getShort("ms_level"));
-					scan.addIon(new Ion(IIon.TIC_ION, spectrumResultSet.getFloat("tic")));
-					double mainPrecursorMZ = spectrumResultSet.getDouble("main_precursor_mz");
-					scan.setPrecursorIon(mainPrecursorMZ);
-					chromatogram.addScan(scan);
-				}
-				spectrumResultSet.close();
+				readMzDB(statement, file, chromatogram);
+				readName(statement, chromatogram);
+				readEditHistory(statement, chromatogram);
+				readSpectrum(statement, chromatogram);
 			}
 		} catch(SQLException e) {
 			logger.warn(e);
 		}
 		monitor.done();
 		return chromatogram;
+	}
+
+	private void readMzDB(Statement statement, File file, IVendorChromatogram chromatogram) throws SQLException {
+
+		try (ResultSet mzdbResultSet = statement.executeQuery("SELECT * FROM mzdb;")) {
+			long epoch = mzdbResultSet.getLong("creation_timestamp") * 1000; // to milliseconds
+			chromatogram.setDate(new Date(epoch));
+			float version = mzdbResultSet.getFloat("version");
+			if(version > 0.7) {
+				logger.warn("The .mzDB file may be incompatible.");
+			}
+		}
+	}
+
+	private void readName(Statement statement, IVendorChromatogram chromatogram) throws SQLException {
+
+		try (ResultSet nameResultSet = statement.executeQuery("SELECT name FROM sample;")) {
+			String sampleName = nameResultSet.getString(1);
+			chromatogram.setSampleName(sampleName);
+		}
+	}
+
+	private List<String> readDataProcessing(Statement statement) throws SQLException {
+
+		List<String> dataProcessing = new ArrayList<>();
+		try (ResultSet dataProcessingSet = statement.executeQuery("SELECT name FROM data_processing ORDER BY id;")) {
+			while(dataProcessingSet.next()) {
+				dataProcessing.add(dataProcessingSet.getString("name"));
+			}
+		}
+		return dataProcessing;
+	}
+
+	private List<String> readSoftware(Statement statement) throws SQLException {
+
+		List<String> software = new ArrayList<>();
+		try (ResultSet softwareResultSet = statement.executeQuery("SELECT name FROM software ORDER BY id;")) {
+			while(softwareResultSet.next()) {
+				software.add(softwareResultSet.getString("name"));
+			}
+		}
+		return software;
+	}
+
+	private void readEditHistory(Statement statement, IVendorChromatogram chromatogram) throws SQLException {
+
+		List<String> dataProcessing = readDataProcessing(statement);
+		List<String> software = readSoftware(statement);
+		//
+		int size = Math.min(dataProcessing.size(), software.size());
+		for(int i = 0; i < size; i++) {
+			chromatogram.getEditHistory().add(new EditInformation(dataProcessing.get(i), software.get(i)));
+		}
+	}
+
+	private void readSpectrum(Statement statement, IVendorChromatogram chromatogram) throws SQLException {
+
+		try (ResultSet spectrumResultSet = statement.executeQuery("SELECT * FROM spectrum;")) {
+			while(spectrumResultSet.next()) {
+				IVendorScan scan = new VendorScan();
+				scan.setScanNumber(spectrumResultSet.getInt("cycle"));
+				scan.setIdentifier(spectrumResultSet.getString("title"));
+				scan.setRetentionTime(Math.round(spectrumResultSet.getFloat("time") * 1000)); // s to ms
+				scan.setMassSpectrometer(spectrumResultSet.getShort("ms_level"));
+				scan.addIon(new Ion(IIon.TIC_ION, spectrumResultSet.getFloat("tic")));
+				double mainPrecursorMZ = spectrumResultSet.getDouble("main_precursor_mz");
+				scan.setPrecursorIon(mainPrecursorMZ);
+				chromatogram.addScan(scan);
+			}
+		}
 	}
 }
