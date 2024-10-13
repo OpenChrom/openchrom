3b74ac920f9c5e33a6c7ae9391dcfad7c931be77
Matthias Mailänder <matthias.mailaender@lablicate.com>
2024-10-13 17:32:12 +0200

    Add .mzMLb export.

---
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mzmlb/src/net/openchrom/msd/converter/supplier/mzmlb/converter/ChromatogramExportConverter.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Lablicate GmbH.
+ *
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Matthias Mailänder - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.msd.converter.supplier.mzmlb.converter;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.chemclipse.converter.chromatogram.AbstractChromatogramExportConverter;
+import org.eclipse.chemclipse.converter.chromatogram.IChromatogramExportConverter;
+import org.eclipse.chemclipse.converter.exceptions.FileIsNotWriteableException;
+import org.eclipse.chemclipse.converter.l10n.ConverterMessages;
+import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.model.core.IChromatogram;
+import org.eclipse.chemclipse.model.core.IPeak;
+import org.eclipse.chemclipse.msd.converter.io.IChromatogramMSDWriter;
+import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.osgi.util.NLS;
+
+import net.openchrom.msd.converter.supplier.mzmlb.io.ChromatogramWriter;
+
+public class ChromatogramExportConverter extends AbstractChromatogramExportConverter implements IChromatogramExportConverter {
+
+	private static final Logger logger = Logger.getLogger(ChromatogramExportConverter.class);
+	private static final String DESCRIPTION = "mzMLb Export Converter";
+
+	@Override
+	public IProcessingInfo<File> convert(File file, IChromatogram<? extends IPeak> chromatogram, IProgressMonitor monitor) {
+
+		IProcessingInfo<File> processingInfo = super.validate(file);
+		if(!processingInfo.hasErrorMessages() && chromatogram instanceof IChromatogramMSD chromatogramMSD) {
+			IChromatogramMSDWriter writer = new ChromatogramWriter();
+			try {
+				writer.writeChromatogram(file, chromatogramMSD, monitor);
+			} catch(IOException e) {
+				logger.warn(e);
+				processingInfo.addErrorMessage(DESCRIPTION, NLS.bind(ConverterMessages.failedToWriteFile, file.getAbsolutePath()));
+			} catch(FileIsNotWriteableException e) {
+				logger.error(e);
+				processingInfo.addErrorMessage(DESCRIPTION, NLS.bind(ConverterMessages.fileNotWritable, file.getAbsolutePath()));
+			}
+			processingInfo.setProcessingResult(file);
+		}
+		return processingInfo;
+	}
+}
