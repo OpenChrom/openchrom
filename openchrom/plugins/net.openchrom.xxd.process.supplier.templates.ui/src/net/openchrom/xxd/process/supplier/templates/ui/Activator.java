61165261cb3d037e9ad2db93c34bfb0faf323c8e
Philip Wenig <philip.wenig@openchrom.net>
2025-04-09 11:52:11 +0200

    Fixed #625 - Unsubscribe registered events

--- openchrom/plugins/net.openchrom.xxd.process.supplier.templates.ui/src/net/openchrom/xxd/process/supplier/templates/ui/Activator.java
+++ openchrom/plugins/net.openchrom.xxd.process.supplier.templates.ui/src/net/openchrom/xxd/process/supplier/templates/ui/Activator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2024 Lablicate GmbH.
+ * Copyright (c) 2018, 2025 Lablicate GmbH.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -52,6 +52,13 @@ public class Activator extends AbstractActivatorUI {
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
+
+		IEventBroker eventBroker = getEventBroker(context);
+		if(eventBroker != null) {
+			for(EventHandler eventHandler : registeredEventHandler) {
+				eventBroker.unsubscribe(eventHandler);
+			}
+		}
 
 		plugin = null;
 		super.stop(context);
