06f8e0ac4e04bf299678009ae86a9e46a894e6bb
Philip Wenig <philip.wenig@openchrom.net>
2025-08-21 08:20:34 +0200

    Fixed #688 - Test Case to calculate the Tanimoto similarity

---
+++ openchrom/tests/net.openchrom.xxd.identifier.supplier.cdk.fragment.test/src/net/openchrom/xxd/identifier/supplier/cdk/converter/TanimotoSimilarity_Test.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Lablicate GmbH.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.xxd.identifier.supplier.cdk.converter;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.fingerprint.CircularFingerprinter;
+import org.openscience.cdk.fingerprint.Fingerprinter;
+import org.openscience.cdk.fingerprint.IBitFingerprint;
+import org.openscience.cdk.fingerprint.IFingerprinter;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.similarity.Tanimoto;
+import org.openscience.cdk.smiles.SmilesParser;
+
+/**
+ * T. T. Tanimoto, (1958) “An elementary mathematical theory of classification and prediction,” IBM Internal Report.
+ */
+public class TanimotoSimilarity_Test {
+
+	private SmilesParser smilesParser = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+
+	private IAtomContainer benzene;
+	private IAtomContainer pyridine;
+
+	@Before
+	public void setUp() throws Exception {
+
+		benzene = smilesParser.parseSmiles("C1=CC=CC=C1");
+		pyridine = smilesParser.parseSmiles("C1=CC=NC=C1");
+	}
+
+	@Test
+	public void test1() throws CDKException {
+
+		IFingerprinter fingerprinter = new Fingerprinter();
+		IBitFingerprint fingerprintBenzene = fingerprinter.getBitFingerprint(benzene);
+		IBitFingerprint fingerprintPyridine = fingerprinter.getBitFingerprint(pyridine);
+		double similarity = Tanimoto.calculate(fingerprintBenzene, fingerprintPyridine);
+		assertEquals(0.2777777777777778d, similarity, 0.0d);
+	}
+
+	@Test
+	public void test2() throws CDKException {
+
+		IFingerprinter fingerprinter = new CircularFingerprinter(CircularFingerprinter.CLASS_ECFP4);
+		IBitFingerprint fingerprintBenzene = fingerprinter.getBitFingerprint(benzene);
+		IBitFingerprint fingerprintPyridine = fingerprinter.getBitFingerprint(pyridine);
+		double similarity = Tanimoto.calculate(fingerprintBenzene, fingerprintPyridine);
+		assertEquals(0.3333333333333333d, similarity, 0.0d);
+	}
+
+	@Test
+	public void test3() throws CDKException {
+
+		IFingerprinter fingerprinter = new CircularFingerprinter(CircularFingerprinter.CLASS_ECFP6);
+		IBitFingerprint fingerprintBenzene = fingerprinter.getBitFingerprint(benzene);
+		IBitFingerprint fingerprintPyridine = fingerprinter.getBitFingerprint(pyridine);
+		double similarity = Tanimoto.calculate(fingerprintBenzene, fingerprintPyridine);
+		assertEquals(0.2727272727272727d, similarity, 0.0d);
+	}
+}
