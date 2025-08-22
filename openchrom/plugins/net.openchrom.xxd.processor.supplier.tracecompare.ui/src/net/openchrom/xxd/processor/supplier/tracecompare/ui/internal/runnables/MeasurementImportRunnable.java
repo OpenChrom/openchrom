a165bac9fc56e0c697eb87672636185b63514e17
Matthias Mailänder <matthias.mailaender@lablicate.com>
2025-08-22 12:05:24 +0200

    Remove unused logger.

--- openchrom/plugins/net.openchrom.xxd.processor.supplier.tracecompare.ui/src/net/openchrom/xxd/processor/supplier/tracecompare/ui/internal/runnables/MeasurementImportRunnable.java
+++ openchrom/plugins/net.openchrom.xxd.processor.supplier.tracecompare.ui/src/net/openchrom/xxd/processor/supplier/tracecompare/ui/internal/runnables/MeasurementImportRunnable.java
@@ -18,7 +18,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.chemclipse.logging.core.Logger;
 import org.eclipse.chemclipse.processing.core.IProcessingInfo;
 import org.eclipse.chemclipse.wsd.converter.chromatogram.ChromatogramConverterWSD;
 import org.eclipse.chemclipse.wsd.model.core.IChromatogramWSD;
@@ -27,13 +26,11 @@ import org.eclipse.jface.operation.IRunnableWithProgress;
 
 public class MeasurementImportRunnable implements IRunnableWithProgress {
 
-	@SuppressWarnings("unused")
-	private static final Logger logger = Logger.getLogger(MeasurementImportRunnable.class);
-
 	private List<IChromatogramWSD> measurements;
 	private List<File> measurementFiles;
 
 	public MeasurementImportRunnable(List<File> measurementFiles) {
+
 		measurements = new ArrayList<>();
 		this.measurementFiles = measurementFiles;
 	}
