71f0dfde7ec1a097af1ea47cadf7496610abdac5
Matthias Mailänder <matthias@mailaender.name>
2025-08-23 10:59:03 +0000

    Fix timezone related unit test failures.

--- openchrom/tests/net.openchrom.msd.converter.supplier.cdf.fragment.test/src/net/openchrom/msd/converter/supplier/cdf/io/support/DateSupport_1_Test.java
+++ openchrom/tests/net.openchrom.msd.converter.supplier.cdf.fragment.test/src/net/openchrom/msd/converter/supplier/cdf/io/support/DateSupport_1_Test.java
@@ -13,190 +13,169 @@
 package net.openchrom.msd.converter.supplier.cdf.io.support;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.text.ParseException;
 import java.util.Date;
+import java.util.TimeZone;
 
-import org.junit.Ignore;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-@Ignore("Fails on CI due to time zones?")
 public class DateSupport_1_Test {
+
+	private TimeZone defaultTimeZone;
+
+	@Before
+	public void setUp() {
+
+		defaultTimeZone = TimeZone.getDefault();
+	}
+
+	@After
+	public void tearDown() {
+
+		TimeZone.setDefault(defaultTimeZone); // restore
+	}
 
 	@Test
 	public void testGetActualDate_1() {
 
 		// 12 Nov 2008 7:41 ! CET > +0100
+		TimeZone.setDefault(TimeZone.getTimeZone("CET"));
 		Date date = new Date(1226472095160l);
 		assertEquals("12 Nov 08 7:41", "20081112074135+0100", DateSupport.getDate(date));
 	}
 
 	@Test
-	public void testGetDate_1() {
+	public void testGetDate_1() throws ParseException {
 
-		try {
-			// 12 Jan 2006 18:47 ! CET > +0100
-			String agilentDate = "20060112184700+0100";
-			Date test = new Date(1137088020000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 12 Jan 2006 18:47 ! CET > +0100
+		String agilentDate = "20060112184700+0100";
+		TimeZone.setDefault(TimeZone.getTimeZone("CET"));
+		Date test = new Date(1137088020000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 
 	@Test
-	public void testGetDate_2() {
+	public void testGetDate_2() throws ParseException {
 
-		try {
-			// 18 Feb 2006 16:18 ! CET > +0100
-			String agilentDate = "20060218161800+0100";
-			Date test = new Date(1140275880000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 18 Feb 2006 16:18 ! CET > +0100
+		String agilentDate = "20060218161800+0100";
+		TimeZone.setDefault(TimeZone.getTimeZone("CET"));
+		Date test = new Date(1140275880000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 
 	@Test
-	public void testGetDate_3() {
+	public void testGetDate_3() throws ParseException {
 
-		try {
-			// 3 Mar 2006 14:26 ! CET > +0100
-			String agilentDate = "20060303142600+0100";
-			Date test = new Date(1141392360000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 3 Mar 2006 14:26 ! CET > +0100
+		String agilentDate = "20060303142600+0100";
+		TimeZone.setDefault(TimeZone.getTimeZone("CET"));
+		Date test = new Date(1141392360000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 
 	@Test
-	public void testGetDate_4() {
+	public void testGetDate_4() throws ParseException {
 
-		try {
-			// 17 Apr 2006 13:40 ! CEST > +0200
-			String agilentDate = "20060417134000+0200";
-			Date test = new Date(1145274000000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 17 Apr 2006 13:40 ! CEST > +0200
+		String agilentDate = "20060417134000+0200";
+		TimeZone.setDefault(TimeZone.getTimeZone("CEST"));
+		Date test = new Date(1145274000000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 
 	@Test
-	public void testGetDate_5() {
+	public void testGetDate_5() throws ParseException {
 
-		try {
-			// 19 May 2006 9:57 ! CEST > +0200
-			String agilentDate = "20060519095700+0200";
-			Date test = new Date(1148025420000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 19 May 2006 9:57 ! CEST > +0200
+		String agilentDate = "20060519095700+0200";
+		TimeZone.setDefault(TimeZone.getTimeZone("CEST"));
+		Date test = new Date(1148025420000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 
 	@Test
-	public void testGetDate_6() {
+	public void testGetDate_6() throws ParseException {
 
-		try {
-			// 16 Jun 2006 21:11 ! CEST > +0200
-			String agilentDate = "20060616211100+0200";
-			Date test = new Date(1150485060000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 16 Jun 2006 21:11 ! CEST > +0200
+		String agilentDate = "20060616211100+0200";
+		TimeZone.setDefault(TimeZone.getTimeZone("CEST"));
+		Date test = new Date(1150485060000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 
 	@Test
-	public void testGetDate_7() {
+	public void testGetDate_7() throws ParseException {
 
-		try {
-			// 28 Jul 2006 18:43 ! CEST > +0200
-			String agilentDate = "20060728184300+0200";
-			Date test = new Date(1154104980000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 28 Jul 2006 18:43 ! CEST > +0200
+		String agilentDate = "20060728184300+0200";
+		TimeZone.setDefault(TimeZone.getTimeZone("CEST"));
+		Date test = new Date(1154104980000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 
 	@Test
-	public void testGetDate_8() {
+	public void testGetDate_8() throws ParseException {
 
-		try {
-			// 22 Aug 2006 16:35 ! CEST > +0200
-			String agilentDate = "20060822163500+0200";
-			Date test = new Date(1156257300000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 22 Aug 2006 16:35 ! CEST > +0200
+		String agilentDate = "20060822163500+0200";
+		TimeZone.setDefault(TimeZone.getTimeZone("CEST"));
+		Date test = new Date(1156257300000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 
 	@Test
-	public void testGetDate_9() {
+	public void testGetDate_9() throws ParseException {
 
-		try {
-			// 1 Sep 2006 13:56 ! CEST > +0200
-			String agilentDate = "20060901135600+0200";
-			Date test = new Date(1157111760000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 1 Sep 2006 13:56 ! CEST > +0200
+		String agilentDate = "20060901135600+0200";
+		TimeZone.setDefault(TimeZone.getTimeZone("CEST"));
+		Date test = new Date(1157111760000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 
 	@Test
-	public void testGetDate_10() {
+	public void testGetDate_10() throws ParseException {
 
-		try {
-			// 17 Oct 2006 1:42 ! CEST > +0200
-			String agilentDate = "20061017014200+0200";
-			Date test = new Date(1161042120000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 17 Oct 2006 1:42 ! CEST > +0200
+		String agilentDate = "20061017014200+0200";
+		TimeZone.setDefault(TimeZone.getTimeZone("CEST"));
+		Date test = new Date(1161042120000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 
 	@Test
-	public void testGetDate_11() {
+	public void testGetDate_11() throws ParseException {
 
-		try {
-			// 9 Nov 2008 20:34 ! CET > +0100
-			String agilentDate = "20081109203400+0100";
-			Date test = new Date(1226259240000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 9 Nov 2008 20:34 ! CET > +0100
+		String agilentDate = "20081109203400+0100";
+		TimeZone.setDefault(TimeZone.getTimeZone("CET"));
+		Date test = new Date(1226259240000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 
 	@Test
-	public void testGetDate_12() {
+	public void testGetDate_12() throws ParseException {
 
-		try {
-			// 22 Dec 2007 6:51 ! CET > +0100
-			String agilentDate = "20071222065100+0100";
-			Date test = new Date(1198302660000l);
-			Date date = DateSupport.getDate(agilentDate);
-			assertTrue(agilentDate, date.equals(test));
-		} catch(ParseException e) {
-			assertTrue("ParseException", false);
-		}
+		// 22 Dec 2007 6:51 ! CET > +0100
+		String agilentDate = "20071222065100+0100";
+		TimeZone.setDefault(TimeZone.getTimeZone("CET"));
+		Date test = new Date(1198302660000l);
+		Date date = DateSupport.getDate(agilentDate);
+		assertEquals(agilentDate, test, date);
 	}
 }
