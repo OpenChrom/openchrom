06ef4aab050103e0ffba63970fcffe77850729b0
Matthias Mailänder <matthias.mailaender@lablicate.com>
2025-10-02 09:06:29 +0200

    Invert the molecule strokes on dark mode.

--- openchrom/plugins/net.openchrom.xxd.identifier.supplier.cdk.ui/src/net/openchrom/xxd/identifier/supplier/cdk/ui/converter/ImageConverter.java
+++ openchrom/plugins/net.openchrom.xxd.identifier.supplier.cdk.ui/src/net/openchrom/xxd/identifier/supplier/cdk/ui/converter/ImageConverter.java
@@ -18,12 +18,17 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 import org.eclipse.chemclipse.logging.core.Logger;
+import org.eclipse.chemclipse.support.ui.workbench.PreferencesSupport;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Display;
 import org.openscience.cdk.depict.DepictionGenerator;
 import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.renderer.generators.BasicSceneGenerator;
+import org.openscience.cdk.renderer.color.CDK2DAtomColors;
+import org.openscience.cdk.renderer.color.IAtomColorer;
+import org.openscience.cdk.renderer.color.UniColor;
+import org.openscience.cdk.renderer.generators.standard.StandardGenerator;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import net.openchrom.xxd.identifier.supplier.cdk.converter.CDKSmilesToMoleculeConverter;
@@ -102,8 +107,30 @@ public class ImageConverter {
 				.withZoom(2.0) //
 				.withBackgroundColor(TRANSPARENT); //
 
-		if(PreferenceSupplier.isColorAtoms()) {
-			depictionGenerator = depictionGenerator.withAtomColors();
+		if(PreferencesSupport.isDarkTheme()) {
+			if(PreferenceSupplier.isColorAtoms()) {
+				depictionGenerator = depictionGenerator.withParam(StandardGenerator.AtomColor.class, new IAtomColorer() {
+
+					private final CDK2DAtomColors base = new CDK2DAtomColors();
+
+					@Override
+					public Color getAtomColor(IAtom atom) {
+
+						if(atom.getAtomicNumber() != null && atom.getAtomicNumber() == IAtom.C) {
+							return Color.WHITE;
+						}
+						return base.getAtomColor(atom);
+					}
+				});
+			} else {
+				depictionGenerator = depictionGenerator.withParam(StandardGenerator.AtomColor.class, new UniColor(Color.WHITE));
+			}
+		} else {
+			if(PreferenceSupplier.isColorAtoms()) {
+				depictionGenerator = depictionGenerator.withParam(StandardGenerator.AtomColor.class, new CDK2DAtomColors());
+			} else {
+				depictionGenerator = depictionGenerator.withParam(StandardGenerator.AtomColor.class, new UniColor(Color.BLACK));
+			}
 		}
 
 		try {
