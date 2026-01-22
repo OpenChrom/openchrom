b40a551240eb1e5ccfddbee24638e622fe2ee1a7
Philip Wenig <philip.wenig@openchrom.net>
2026-01-22 09:11:32 +0100

    Update copyright header.

--- openchrom/plugins/net.openchrom.installer/src/net/openchrom/installer/preferences/PreferenceSupplier.java
+++ openchrom/plugins/net.openchrom.installer/src/net/openchrom/installer/preferences/PreferenceSupplier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2025 Lablicate GmbH.
+ * Copyright (c) 2023, 2026 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
