dc6819f84d1361c4dc923e23d03f8e3548b7aae5
Matthias Mailänder <matthias.mailaender@lablicate.com>
2026-02-17 14:27:02 +0100

    Don't log a warning when there is no crossing point.

--- openchrom/plugins/net.openchrom.xxd.process.supplier.templates/src/net/openchrom/xxd/process/supplier/templates/peaks/PeakTransfer.java
+++ openchrom/plugins/net.openchrom.xxd.process.supplier.templates/src/net/openchrom/xxd/process/supplier/templates/peaks/PeakTransfer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2025 Lablicate GmbH.
+ * Copyright (c) 2020, 2026 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -313,7 +313,6 @@ public class PeakTransfer extends AbstractPeakDetector implements IPeakDetectorM
 		try {
 			return solver.solve(1000, h, min, max);
 		} catch(TooManyEvaluationsException | NoBracketingException e) {
-			logger.warn(e);
 			return null;
 		}
 	}
