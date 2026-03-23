0640e56caa8cd6bf761a36f40a71ed5f461dc016
Александър Куртаков <akurtakov@gmail.com>
2026-03-23 12:40:55 +0200

    Remove redundant code
    * Remove redundant implements
    * Lambda conversion

--- openchrom/plugins/net.openchrom.xxd.process.supplier.templates.ui/src/net/openchrom/xxd/process/supplier/templates/ui/adapter/CompensationQuantifierSettingsAdapterFactory.java
+++ openchrom/plugins/net.openchrom.xxd.process.supplier.templates.ui/src/net/openchrom/xxd/process/supplier/templates/ui/adapter/CompensationQuantifierSettingsAdapterFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2025 Lablicate GmbH.
+ * Copyright (c) 2020, 2026 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -13,12 +13,8 @@
  *******************************************************************************/
 package net.openchrom.xxd.process.supplier.templates.ui.adapter;
 
-import java.io.IOException;
-
-import org.eclipse.chemclipse.processing.supplier.IProcessorPreferences;
 import org.eclipse.chemclipse.ux.extension.ui.methods.SettingsUIProvider;
 import org.eclipse.core.runtime.IAdapterFactory;
-import org.eclipse.swt.widgets.Composite;
 
 import net.openchrom.xxd.process.supplier.templates.settings.CompensationQuantifierSettings;
 import net.openchrom.xxd.process.supplier.templates.ui.swt.CompensationQuantifierEditor;
@@ -38,14 +34,10 @@ public class CompensationQuantifierSettingsAdapterFactory implements IAdapterFac
 
 	private static SettingsUIProvider<CompensationQuantifierSettings> createSettingsUIProvider(CompensationQuantifierSettings adaptedSettings) {
 
-		return new SettingsUIProvider<CompensationQuantifierSettings>() {
+		return (parent, preferences, showProfileToolbar) -> {
 
-			@Override
-			public SettingsUIProvider.SettingsUIControl createUI(Composite parent, IProcessorPreferences<CompensationQuantifierSettings> preferences, boolean showProfileToolbar) throws IOException {
-
-				CompensationQuantifierSettings userSettings = preferences.getUserSettings();
-				return new CompensationQuantifierEditor(parent, preferences, userSettings == null ? adaptedSettings : userSettings);
-			}
+			CompensationQuantifierSettings userSettings = preferences.getUserSettings();
+			return new CompensationQuantifierEditor(parent, preferences, userSettings == null ? adaptedSettings : userSettings);
 		};
 	}
 
