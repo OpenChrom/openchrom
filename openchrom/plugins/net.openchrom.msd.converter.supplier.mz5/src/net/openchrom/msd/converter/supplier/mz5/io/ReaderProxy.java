9e950688c1c7464776e48f0c2e119b8f00f9e43b
Matthias Mailänder <matthias.mailaender@lablicate.com>
2026-06-18 19:40:44 +0200

    Don't check ions for performance reasons.

--- openchrom/plugins/net.openchrom.msd.converter.supplier.mz5/src/net/openchrom/msd/converter/supplier/mz5/io/ReaderProxy.java
+++ openchrom/plugins/net.openchrom.msd.converter.supplier.mz5/src/net/openchrom/msd/converter/supplier/mz5/io/ReaderProxy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2025 Lablicate GmbH.
+ * Copyright (c) 2021, 2026 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -46,7 +46,7 @@ public class ReaderProxy implements IReaderProxy {
 				} else {
 					ion = new VendorIon(mz, intensity);
 				}
-				massSpectrum.addIon(ion);
+				massSpectrum.addIon(ion, false);
 			}
 		} catch(OutOfMemoryError e) {
 			logger.error(e);
