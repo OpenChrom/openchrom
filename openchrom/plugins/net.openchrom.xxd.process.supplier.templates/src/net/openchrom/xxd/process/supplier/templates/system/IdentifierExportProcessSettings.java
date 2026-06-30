7fa838d0db68f5513ec2ac6ca2064de0a264e643
Philip Wenig <philip.wenig@openchrom.net>
2026-06-30 16:37:42 +0200

    Add option to select the position directive for identifier export.

--- openchrom/plugins/net.openchrom.xxd.process.supplier.templates/src/net/openchrom/xxd/process/supplier/templates/system/IdentifierExportProcessSettings.java
+++ openchrom/plugins/net.openchrom.xxd.process.supplier.templates/src/net/openchrom/xxd/process/supplier/templates/system/IdentifierExportProcessSettings.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2025 Lablicate GmbH.
+ * Copyright (c) 2021, 2026 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -13,11 +13,13 @@
 package net.openchrom.xxd.process.supplier.templates.system;
 
 import org.eclipse.chemclipse.processing.system.ISystemProcessSettings;
+import org.eclipse.chemclipse.support.settings.DoubleSettingsProperty;
 import org.eclipse.chemclipse.support.settings.IntSettingsProperty;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
+import net.openchrom.xxd.process.supplier.templates.model.PositionDirective;
 import net.openchrom.xxd.process.supplier.templates.preferences.PreferenceSupplier;
 
 public class IdentifierExportProcessSettings implements ISystemProcessSettings {
@@ -26,14 +28,17 @@ public class IdentifierExportProcessSettings implements ISystemProcessSettings {
 	@IntSettingsProperty(minValue = PreferenceSupplier.MIN_NUMBER_TRACES, maxValue = PreferenceSupplier.MAX_NUMBER_TRACES)
 	@JsonPropertyDescription(value = "Select the number of highest traces to be exported.")
 	private int numberTraces = 0;
-	@JsonProperty(value = "Delta Left [ms]", defaultValue = "0")
-	@IntSettingsProperty(minValue = PreferenceSupplier.MIN_DELTA_MILLISECONDS, maxValue = PreferenceSupplier.MAX_DELTA_MILLISECONDS)
-	@JsonPropertyDescription(value = "Extend the left review range by the given value in milliseconds.")
-	private int retentionTimeDeltaLeft = 0;
-	@JsonProperty(value = "Delta Right [ms]", defaultValue = "0")
-	@IntSettingsProperty(minValue = PreferenceSupplier.MIN_DELTA_MILLISECONDS, maxValue = PreferenceSupplier.MAX_DELTA_MILLISECONDS)
-	@JsonPropertyDescription(value = "Extend the right review range by the given value in milliseconds.")
-	private int retentionTimeDeltaRight = 0;
+	@JsonProperty(value = "Position Directive (Coordinate)", defaultValue = "RETENTION_TIME_MIN")
+	@JsonPropertyDescription(value = "Select whether to use Minutes, Milliseconds or Retention Index.")
+	private PositionDirective positionDirective = PositionDirective.RETENTION_TIME_MIN;
+	@JsonProperty(value = "Delta Left [Coordinate]", defaultValue = "0")
+	@DoubleSettingsProperty(minValue = PreferenceSupplier.MIN_DELTA_COORDINATE, maxValue = PreferenceSupplier.MAX_DELTA_COORDINATE)
+	@JsonPropertyDescription(value = "Extend the left review range by the given coordinate value.")
+	private double coordinateDeltaLeft = 0;
+	@JsonProperty(value = "Delta Right [Coordinate]", defaultValue = "0")
+	@DoubleSettingsProperty(minValue = PreferenceSupplier.MIN_DELTA_COORDINATE, maxValue = PreferenceSupplier.MAX_DELTA_COORDINATE)
+	@JsonPropertyDescription(value = "Extend the right review range by the given coordinate value.")
+	private double coordinateDeltaRight = 0;
 
 	public int getNumberTraces() {
 
@@ -45,23 +50,33 @@ public class IdentifierExportProcessSettings implements ISystemProcessSettings {
 		this.numberTraces = numberTraces;
 	}
 
-	public int getRetentionTimeDeltaLeft() {
+	public PositionDirective getPositionDirective() {
 
-		return retentionTimeDeltaLeft;
+		return positionDirective;
 	}
 
-	public void setRetentionTimeDeltaLeft(int retentionTimeDeltaLeft) {
+	public void setPositionDirective(PositionDirective positionDirective) {
 
-		this.retentionTimeDeltaLeft = retentionTimeDeltaLeft;
+		this.positionDirective = positionDirective;
 	}
 
-	public int getRetentionTimeDeltaRight() {
+	public double getCoordinateDeltaLeft() {
 
-		return retentionTimeDeltaRight;
+		return coordinateDeltaLeft;
 	}
 
-	public void setRetentionTimeDeltaRight(int retentionTimeDeltaRight) {
+	public void setCoordinateDeltaLeft(double coordinateDeltaLeft) {
 
-		this.retentionTimeDeltaRight = retentionTimeDeltaRight;
+		this.coordinateDeltaLeft = coordinateDeltaLeft;
+	}
+
+	public double getCoordinateDeltaRight() {
+
+		return coordinateDeltaRight;
+	}
+
+	public void setCoordinateDeltaRight(double coordinateDeltaRight) {
+
+		this.coordinateDeltaRight = coordinateDeltaRight;
 	}
 }
