3e758597e1804463ef1ceb8e4870f2f69650444e
Matthias Mailänder <matthias.mailaender@lablicate.com>
2026-08-21 19:23:41 +0200

    Add OpenChrom platform tests.

---
+++ openchrom/tests/net.openchrom.msd.converter.supplier.cdf.fragment.test/src/net/openchrom/msd/converter/supplier/cdf/io/ChromatogramReader_TEST_UC_1_ITest.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2026 Lablicate GmbH.
+ * 
+ * All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.msd.converter.supplier.cdf.io;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.io.File;
+
+import org.eclipse.chemclipse.msd.model.core.IChromatogramMSD;
+import org.eclipse.chemclipse.processing.core.IProcessingInfo;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import net.openchrom.msd.converter.supplier.cdf.converter.ChromatogramImportConverter;
+
+/**
+ * Tests if lower case directories will be recognized.
+ */
+@TestInstance(Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ChromatogramReader_TEST_UC_1_ITest {
+
+	private IChromatogramMSD chromatogram;
+
+	@Test
+	@Order(1)
+	public void testImport() {
+
+		File fileImport = new File("testData/TEST_UC.CDF");
+		ChromatogramImportConverter importConverter = new ChromatogramImportConverter();
+		IProcessingInfo<IChromatogramMSD> processingInfo = importConverter.convert(fileImport, new NullProgressMonitor());
+		chromatogram = processingInfo.getProcessingResult();
+	}
+
+	@Test
+	public void testNotReadable() {
+
+		assertNull(chromatogram);
+	}
+}
